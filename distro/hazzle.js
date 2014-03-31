edb6bbcac1e266d86073e5720357d477ed46ca07
KFlash <kflash123@gmail.com>
2014-03-31 23:10:35 +0800

    Added json and xml parsing

--- distro/hazzle.js
+++ distro/hazzle.js
@@ -3027,6 +3027,44 @@
 
     });
 
+    // **************************************************************
+    // Parsing
+    // **************************************************************
+
+    hAzzle.extend({
+
+        /**
+         * Cross-browser JSON parsing
+         *
+         * @param {String} data
+         */
+
+        parseJSON: function (data) {
+            return JSON.parse(data + "");
+        },
+
+        parseXML: function (data) {
+            var xml, tmp;
+            if (!data || typeof data !== "string") {
+                return null;
+            }
+
+            // Support: IE9
+            try {
+                tmp = new DOMParser();
+                xml = tmp.parseFromString(data, "text/xml");
+            } catch (e) {
+                xml = undefined;
+            }
+
+            if (!xml || xml.getElementsByTagName("parsererror").length) {
+                return new Error("Invalid XML: " + data);
+            }
+            return xml;
+        }
+
+    });
+
     window['hAzzle'] = hAzzle;
 
 })(window);
