3a7c7012644de3a205ca1fcfa44c8bcebc8b2db5
KFlash <kflash123@gmail.com>
2014-04-13 13:50:48 +0800

    added nextOfClass

--- modules/traversing.js
+++ modules/traversing.js
@@ -176,12 +176,49 @@ hAzzle.fn.extend({
     next: function (selector) {
         return selector ? hAzzle(this.pluckNode('nextSibling').filter(selector)) : hAzzle(this.pluckNode('nextSibling'));
     },
+	
+	/**
+	 * Find the next class after given element.
+	 *
+	 * @param {String} className
+	 * @return {Object}
+	 *
+	 **/
+	
+	nextOfClass: function(className) {
+    var nextEl,
+        el = this;
+		
+    // Leading period will confuse hAzzle. 
+    
+	if (className[0] === '.') className = className.slice(1); 
+
+    while (el.next()) {
+
+        // If target element is found, stop
+        if (el.hasClass(className)) return el; 
+
+        nextEl = el.next(); 
+        if (nextEl.length === 0) {
+            // No more siblings. Go up in DOM and restart loop to check parent
+            el = el.parent(); 
+            continue; 
+        }
+
+        el = nextEl;  
+
+         // End of doc. Give up. 
+        if (el.parent().length === 0) return false;
+    }
+	
+	},
 
     /**
      *  Return the element's previous sibling
      *
      * @return {Object}
      */
+
 
     prev: function (selector) {
         return selector ? hAzzle(this.pluckNode('previousSibling').filter(selector)) : hAzzle(this.pluckNode('previousSibling'));
@@ -255,12 +292,12 @@ hAzzle.each({
 
     hAzzle.fn[name] = function (sel) {
         var els = hAzzle(),
-            el = this[subn]();
+            el = this[subn](); // next() or prev()
         while (el.length) {
             if (typeof sel === 'undefined' || el.is(sel)) {
                 els = els.add(el);
             }
-            el = el[subn]();
+            el = el[subn](); // next() or prev()
         }
         return els;
     };
