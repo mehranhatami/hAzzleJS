62f4e3d7dca574d40c4b4f49d7402b4fca871574
KFlash <kflash123@gmail.com>
2014-04-17 14:39:01 +0800

    localeStorage fix

--- modules/localestorage.js
+++ modules/localestorage.js
@@ -1,362 +1,201 @@
-/**
- * Locale storage
- * HTML5 API
- */
- 
-var storage = {},
-    storage_size = 0,
-    timeout = null;
+// Common 5MB localStorage limit among current browsers
 
-
-/**
- * Save data to the localeStorage
- */
-function saveIt() {
-    localStorage.hAzzle_storage = JSON.stringify(storage);
-    storage_size = localStorage.hAzzle_storage ? String(localStorage.hAzzle_storage).length : 0;
-    return true;
-}
-
-
-/**
- * Reload data from storage when needed
- */
-function reloadData() {
-    load_storage();
-    removeStorageKeys();
-}
-
-
-function load_storage() {
-
-    var source = localStorage.hAzzle_storage;
-
-    storage = hAzzle.parseJSON(source) || {};
-
-    storage_size = source ? String(source).length : 0;
-}
-
-/**
- * Remove key from the storage
- */
-
-
-function removeStorageKeys() {
-
-    var curtime, i, len, expire, keys, nextExpire = Infinity,
-        expiredKeysCount = 0;
-
-    clearTimeout(timeout);
-
-    if (!storage || !storage.__hAzzle_storage || !storage.__hAzzle_storage.TTL) {
-        return;
-    }
-
-    curtime = +new Date();
-    keys = storage.__hAzzle_storage.TTL.keys || [];
-    expire = storage.__hAzzle_storage.TTL.expire || {};
-
-    for (i = 0, len = keys.length; i < len; i++) {
-        if (expire[keys[i]] <= curtime) {
-            expiredKeysCount++;
-            delete storage[keys[i]];
-            delete expire[keys[i]];
-        } else {
-            if (expire[keys[i]] < nextExpire) {
-                nextExpire = expire[keys[i]];
-            }
-            break;
-        }
-    }
-
-    // set next check
-    if (nextExpire !== Infinity) {
-        timeout = setTimeout(removeStorageKeys, Math.min(nextExpire - curtime, 0x7FFFFFFF));
-    }
-
-    // remove expired from TTL list and save changes
-    if (expiredKeysCount) {
-        keys.splice(0, expiredKeysCount);
-
-        cleanMetaObject();
-        saveIt();
-    }
-}
-
-
-
-
-function setTTL(key, ttl) {
-    var curtime = +new Date(),
-        i, len, added = false;
-
-    ttl = Number(ttl) || 0;
-
-    // Set TTL value for the key
-    if (ttl !== 0) {
-        // If key exists, set TTL
-        if (storage.hasOwnProperty(key)) {
-
-            if (!storage.__hAzzle_storage) {
-                storage.__hAzzle_storage = {};
-            }
-
-            if (!storage.__hAzzle_storage.TTL) {
-                storage.__hAzzle_storage.TTL = {
-                    expire: {},
-                    keys: []
-                };
-            }
-
-            storage.__hAzzle_storage.TTL.expire[key] = curtime + ttl;
-
-            // remove from keys array
-            if (storage.__hAzzle_storage.TTL.expire.hasOwnProperty(key)) {
-                for (i = 0, len = storage.__hAzzle_storage.TTL.keys.length; i < len; i++) {
-                    if (storage.__hAzzle_storage.TTL.keys[i] == key) {
-                        storage.__hAzzle_storage.TTL.keys.splice(i);
-                    }
-                }
-            }
-
-            // add to keys array, sorted by ttl
-            for (i = 0, len = storage.__hAzzle_storage.TTL.keys.length; i < len; i++) {
-                if (storage.__hAzzle_storage.TTL.expire[storage.__hAzzle_storage.TTL.keys[i]] > curtime + ttl) {
-                    storage.__hAzzle_storage.TTL.keys.splice(i, 0, key);
-                }
-            }
-
-            if (!added) {
-                storage.__hAzzle_storage.TTL.keys.push(key);
-            }
-        } else {
-            return false;
-        }
-    } else {
-        // Remove TTL if set
-        if (storage && storage.__hAzzle_storage && storage.__hAzzle_storage.TTL) {
-
-            if (storage.__hAzzle_storage.TTL.expire.hasOwnProperty(key)) {
-                delete storage.__hAzzle_storage.TTL.expire[key];
-                for (i = 0, len = storage.__hAzzle_storage.TTL.keys.length; i < len; i++) {
-                    if (storage.__hAzzle_storage.TTL.keys[i] == key) {
-                        storage.__hAzzle_storage.TTL.keys.splice(i, 1);
-                        break;
-                    }
-                }
-            }
-
-            cleanMetaObject();
-        }
-    }
-
-    // schedule next TTL check
-    clearTimeout(timeout);
-    if (storage && storage.__hAzzle_storage && storage.__hAzzle_storage.TTL && storage.__hAzzle_storage.TTL.keys.length) {
-        timeout = setTimeout(removeStorageKeys, Math.min(Math.max(storage.__hAzzle_storage.TTL.expire[storage.__hAzzle_storage.TTL.keys[0]] - curtime, 0), 0x7FFFFFFF));
-    }
-
-    return true;
-}
-
-function cleanMetaObject() {
-    var updated = false,
-        hasProperties = false,
-        i;
-
-    if (!storage || !storage.__hAzzle_storage) {
-        return updated;
-    }
-
-    // If nothing to TTL, remove the object
-    if (storage.__hAzzle_storage.TTL && !storage.__hAzzle_storage.TTL.keys.length) {
-        delete storage.__hAzzle_storage.TTL;
-        updated = true;
-    }
-
-    // If meta object is empty, remove it
-    for (i in storage.__hAzzle_storage) {
-        if (storage.__hAzzle_storage.hasOwnProperty(i)) {
-            hasProperties = true;
-            break;
-        }
-    }
-
-    if (!hasProperties) {
-        delete storage.__hAzzle_storage;
-        updated = true;
-    }
-
-    return updated;
-}
+var isObject = hAzzle.isObject,
+    defaultSize = 5242880;
 
 hAzzle.extend({
 
     /**
-     * Retrive data from localeStorage during page refresh
+     * Convert bytes to human readable KB / MB / GB
      */
 
-    pageLoad: function () {
-
-        // Load data from storage
-        load_storage();
-
-        // remove dead keys
-        removeStorageKeys();
-
-        // start listening for changes
-
-        hAzzle(window).on("storage", reloadData);
-
-        // Remove the listener after loading
-
-        hAzzle(window).one('pageshow', function (event) {
-
-            if (event.persisted) {
-                reloadData();
-            }
-        }, false);
+    bytesToSize: function (bytes) {
+        var k = 1000;
+        var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+        if (bytes === 0) return '0 Bytes';
+        var i = parseInt(Math.floor(Math.log(bytes) / Math.log(k)), 10);
+        return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
     },
 
-    /** Store, update or retrive a value in the local storage.
-     *
-     * @param {String} key
-     * @param {String} value
-     * @param {Number} ttl
+    /**
+     * Removes all key / value pairs from localStorage
+     */
+
+    clearStorage: function clear() {
+        localStorage.clear();
+    },
+
+    /**
+     * Returns an array of keys currently stored in localStorage.
+     */
+
+    storageContains: function (key) {
+
+        if (typeof key === 'string') {
+            return hAzzle.indexOf(this.getStorageKeys(), key) !== -1;
+        }
+    },
+
+    /**
+     * Returns an array of keys currently stored in localStorage.
+     */
+
+    getStorageKeys: function () {
+
+        var result = [],
+            i = 0;
+
+        for (i = localStorage.length; i--;) {
+            result.push(localStorage.key(i));
+        }
+
+        return result;
+    },
+
+    /**
+     * Returns an approximation of how much space is left in localStorage
+     */
+
+    getRemainingStorageSpace: function () {
+        return this.bytesToSize(defaultSize - this.getStorageSize(true));
+    },
+
+    /**
+     * Returns the size of the total contents in localStorage.
      *
      */
 
-    storage: function (key, value, ttl) {
+    getStorageSize: function ( /*INTERNAL*/ pure) {
 
-        var len = arguments.length;
+        if (pure) {
 
-        if (len === 1) {
+            return JSON.stringify(localStorage).length;
 
-            if (storage.hasOwnProperty(key) && key != "__hAzzle_storage") {
-                // TTL value for an existing key is either a positive number or an Infinity
-                if (this.getTTL(key)) {
-                    return storage[key] && storage[key];
+        } else { // Human readable
+
+            return this.bytesToSize(JSON.stringify(localStorage).length);
+        }
+    },
+
+    /**
+     *  Returns true if localStorage has no key/value pairs
+     */
+
+    isStorageEmpty: function () {
+        return this.getStorageKeys().length === 0;
+    },
+
+    /**
+     * Removes the specified key/value pair
+     */
+
+    removeStorage: function (key) {
+
+        if (typeof key === 'string') {
+
+            localStorage.removeItem(key);
+
+        } else if (key instanceof Array) {
+
+            for (var i = key.length; i--;) {
+
+                if (typeof key[i] === 'string') {
+
+                    localStorage.removeItem(key[i]);
                 }
             }
-
-
-        } else if (len >= 2) {
-
-            if (key == "__hAzzle_storage") {
-                return false;
-            }
-
-            if (!storage) {
-                return false;
-            }
-
-            // undefined values are deleted automatically
-            if (typeof value == "undefined") {
-                return this.deleteKey(key);
-            }
-
-            // Check if the value is JSON compatible (and remove reference to existing objects/arrays)
-
-            if (hAzzle.isObject(value)) {
-
-                value = hAzzle.parseJSON(JSON.stringify(value));
-            }
-
-            storage[key] = value;
-
-            if (ttl) {
-
-                setTTL(key, ttl || 0);
-
-            }
-
-            return saveIt();
         }
-
-        return;
     },
 
     /**
-     * Delete key from the Storage
+     * Returns the proper-type value of a specified key
+     */
+    getStorage: function (key, defaultValue) {
+
+        if (typeof key === 'string') {
+
+            var value = localStorage.getItem(key).toLowerCase(), // retrieve value
+                number = parseFloat(value); // to allow for number checking
+
+            if (value === null) {
+                // Returns default value if key is not set, otherwise returns null
+                return arguments.length === 2 ? defaultValue : null;
+            }
+
+            if (!hAzzle.isNaN(number)) {
+                return number; // value was of type number
+            }
+
+            if (value === 'true' || value === 'false') {
+                return value === 'true'; //value was of type boolean
+            }
+
+            try {
+                value = hAzzle.parseJSON(value);
+                return value;
+            } catch (e) {
+                return value;
+            }
+        }
+
+    },
+
+    /**
+     * Stores a given object in localStorage, allowing access to individual object properties
+     **/
+
+    setStorage: function (key, value) {
+
+        if (arguments.length === 1) {
+
+            this.store(key);
+
+        } else if (typeof key === 'string') {
+
+            if (isObject(value)) {
+
+                value = JSON.stringify(value);
+            }
+
+            localStorage.setItem(key, value);
+
+        }
+    },
+
+    /**
+     * Saves a given object in localStorage, allowing access to individual object properties
+     **/
+
+    saveStorage: function (value) {
+        var property;
+
+        if (isObject(value) && !(value instanceof Array)) {
+            for (property in value) {
+                localStorage.setItem(property, value[property]);
+            }
+        }
+    },
+
+
+    /**
+     * Returns an object representation of the current state of localStorage
      *
-     * @param {String} key
      */
 
-    deleteKey: function (key) {
-        return storage ? key in storage ? (delete storage[key], setTTL(key, 0), saveIt()) : false : false;
-    },
+    StorageToObject: function () {
 
-    /**
-     * Set a millisecond timeout on a key in the Storage
-     *
-     * @param {String} key
-     * @param {Number} ttl
-     */
+        var o = {},
+            keys = this.getStorageKeys(),
+            i = 0,
+            len = keys.length;
 
-    setTTL: function (key, ttl) {
-      return storage ? (setTTL(key, ttl), saveIt()) : false; 
-    },
-
-    /**
-     * Retrieve remaining milliseconds for a key with TTL
-     *
-     * @param {String} key
-     */
-
-    getTTL: function (key) {
-        var ttl;
-
-        if (!storage) {
-            return false;
+        for (i = len; i--;) {
+            o[keys[i]] = this.getStorage(keys[i]);
         }
 
-        if (storage.hasOwnProperty(key)) {
-            if (storage.__hAzzle_storage &&
-                storage.__hAzzle_storage.TTL &&
-                storage.__hAzzle_storage.TTL.expire &&
-                storage.__hAzzle_storage.TTL.expire.hasOwnProperty(key)) {
-
-                ttl = Math.max(storage.__hAzzle_storage.TTL.expire[key] - (+new Date()) || 0, 0);
-
-                return ttl || false;
-            } else {
-                return Infinity;
-            }
-        }
-
-        return false;
-    },
-
-    /**
-     * Clear all values
-     * @return {Array}
-     */
-
-    flush: function () {
-     return storage ? (storage = {}, localStorage.removeItem("hAzzle_storage"), true) : false;
-    },
-
-    /**
-     * Retrieve all used keys as an array
-     */
-
-    storageIndex: function () {
-        if (!storage) {
-            return false;
-        }
-
-        var index = [],
-            i;
-        for (i in storage) {
-            if (storage.hasOwnProperty(i) && i != "__hAzzle_storage") {
-                index.push(i);
-            }
-        }
-        return index;
-    },
-
-    storageSize: function () {
-        return storage_size;
+        return o;
     }
+
+
 
 });
