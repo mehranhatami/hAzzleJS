28060fddb0f75a9c4e05c4dd1e24146c113076e1
KFlash <kflash123@gmail.com>
2014-04-27 13:02:05 +0800

    Fixed the distro build

--- distro/hazzle.js
+++ distro/hazzle.js
@@ -5225,6 +5225,8 @@ $.each(["margin", "padding"], function (i, hook) {
          * Set text for every element in the collection
          *
          * $('div').text() => div text
+
+
          *
          * @param {String} value
          * @param {String} dir
@@ -5730,5 +5732,65 @@ $.each(["margin", "padding"], function (i, hook) {
         $.propMap[this.toLowerCase()] = this;
     });
 
+
+})(hAzzle);
+
+
+// Removeable
+
+; (function ($) {
+
+    /**
+     * Remove all child nodes of the set of matched elements from the DOM.
+     *
+     * @return {Object}
+     */
+
+ $.fn.empty = function () {
+       
+	   // Remove all data to prevent memory leaks
+	   
+        return this.removeData().each(function (_, elem) {
+			
+         if ( $.nodeType(1, this)) {
+
+			$.Events.remove(elem);
+			 
+		 // Remove any remaining nodes
+        
+		 this.textContent = "";
+		 }
+        });
+    },
+	
+	 /**
+     *  Remove an element from the DOM
+     */
+ $.fn.remove = function () {
+
+		// Discard any data on the element
+
+        return this.removeData().each(function (_, elem) {
+			
+		// Locate all nodes that belong to this element
+		// and add them to the "elems stack"
+			
+		  var elements = $(elem).find('*');
+		      elements  = elements.add(elem);
+		
+			$.Events.remove(elem);
+        
+		 var parent = elem.parentNode;
+		 
+        if (parent) {
+
+		// Remove all children
+
+	     this.parentNode.removeChild(elem);
+		}
+        
+       })
+	   return false;
+    }
 
 })(hAzzle);
