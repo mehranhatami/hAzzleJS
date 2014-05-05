c615e182caf12890151c3c73cc8808c782b15ae6
KFlash <kflash123@gmail.com>
2014-05-05 11:36:18 +0800

    v. 0.4d Beta
    Cleaned up the traversing.js and increased the performance. No doubt,
    hAzzle are the fastest one.

--- modules/traversing.js
+++ modules/traversing.js
@@ -1,358 +1,338 @@
 /*!
  * Traversing.js
  */
+var cached = [],
 
-    var cached = [];
+    guaranteedUnique = {
+        children: true,
+        contents: true,
+        next: true,
+        prev: true
+    },
 
-    hAzzle.extend(hAzzle.fn, {
+    slice = Array.prototype.slice;
 
-        /**
-         * Fetch property from elements
-         *
-         * @param {String} prop
-         * @return {Array}
-         */
-
-        pluckNode: function (prop) {
-            return this.map(function (element) {
-                return hAzzle.getClosestNode(element, prop);
-            });
-        },
-
-        /**
-         * Get the  element that matches the selector, beginning at the current element and progressing up through the DOM tree.
-         *
-         * @param {String} sel
-         * @return {Object}
-         */
-
-        closest: function (sel, ctx) {
-            return this.map(function (elem) {
-                if (hAzzle.nodeType(1, elem) && elem !== ctx && !hAzzle.isDocument(elem) && hAzzle.matches(elem, typeof sel === 'object' ? hAzzle(sel) : sel)) {
-                    return elem;
-                }
-                return hAzzle.getClosestNode(elem, 'parentNode', sel, /* NodeType 11 */ 11);
-            });
-        },
-
-        /** Determine the position of an element within the matched set of elements
-         *
-         * @param {string} elem
-         * @param {return} Object
-         */
-
-        index: function (elem) {
-            return elem ? this.indexOf(hAzzle(elem)[0]) : this.parent().children().indexOf(this[0]) || -1;
-        },
-
-        /**
-         *  Pick elements by tagNames from the "elems stack"
-         *
-         * @param {string} tag
-         * @return {Object}
-         */
-        tags: function (tag) {
-            return this.map(function (els) {
-                if (els.tagName.toLowerCase() === tag && hAzzle.nodeType(1, els)) {
-                    return els;
-                }
-            });
-        },
-
-        /**
-         * Adds one element to the set of matched elements.
-         *
-         * @param {String} sel
-         * @param {String} ctx
-         * @return {Object}
-         */
-
-        add: function (sel, ctx) {
-            return this.concat(hAzzle(sel, ctx).elems);
-        },
-
-        /**
-         * Reduce the set of matched elements to those that have a descendant that matches the selector or DOM element.
-         */
-        has: function (target) {
-
-            var targets = hAzzle(target, this),
-                i = 0,
-                l = targets.length;
-
-            return this.filter(function () {
-                for (; i < l; i++) {
-                    if (hAzzle.contains(this, targets[i])) {
-                        return true;
-                    }
-                }
-            });
-        },
-
-        /**
-         * Remove elements from the set of matched elements.
-         *
-         * @param {String} sel
-         * @return {Object}
-         *
-         */
-
-        not: function (sel) {
-            return this.filter(sel, true);
-        },
-
-        /**
-         * Check if the first element in the element collection matches the selector
-         *
-         * @param {String|Object} sel
-         * @return {Boolean}
-         */
-
-        is: function (sel) {
-            return !!sel && (
-                /^[\x20\t\r\n\f]*[>+~]|:(even|odd|eq|gt|lt|nth|first|last)(?:\([\x20\t\r\n\f]*((?:-\d)?\d*)[\x20\t\r\n\f]*\)|)(?=[^-]|$)/i.test(sel) ?
-                hAzzle(sel).index(this[0]) >= 0 :
-                this.filter(sel).length > 0);
-        },
-
-        /**
-         * Get immediate parents of each element in the collection.
-         * If CSS selector is given, filter results to include only ones matching the selector.
-         *
-         * @param {String} sel
-         * @return {Object}
-         */
-
-        parent: function (sel) {
-            return hAzzle.create(this.pluck('parentNode', /* NodeType 11 */ 11), sel);
-        },
-
-        /**
-         *  Get the ancestors of each element in the current set of matched elements
-         *
-         * @param {String} sel
-         * @return {Object}
-         */
-
-        parents: function (sel) {
-            var ancestors = [],
-                elements = this.elems,
-                fn = function (element) {
-                    if ((element = element.parentNode) && element !== document && ancestors.indexOf(element) < 0) {
-                        if (hAzzle.nodeType(1, element)) {
-                            ancestors.push(element);
-                            return element;
-                        }
-                    }
-                };
-
-            while (elements.length > 0 && elements[0] !== undefined) {
-                elements = elements.map(fn);
-            }
-
-            return hAzzle.create(ancestors, sel);
-        },
-
-        /**
-         * Get all decending elements of a given element
-         * If selector is given, filter the results to only include ones matching the CSS selector.
-         *
-         * @param {String} sel
-         * @return {Object}
-         */
-
-        children: function (sel) {
-            return hAzzle(this.reduce(function (els, elem) {
-                if (hAzzle.nodeType(1, elem)) {
-                    return els.concat(slice.call(elem.children));
-                }
-            }, []), sel);
-
-        },
-
-        /**
-         *  Return the element's next sibling
-         *
-         * @return {Object}
-         */
-
-        next: function (selector) {
-            return selector ? hAzzle(this.pluckNode('nextSibling').filter(selector || [])) : hAzzle(this.pluckNode('nextSibling'));
-        },
-
-        nextUntil: function (until) {
-
-            var matches = [];
-
-            this.nextAll().each(function () {
-                if (hAzzle(this).is(until)) return false;
-                matches.push(this);
-            });
-
-            return hAzzle(matches);
-        },
-
-        /**
-         *  Return the element's previous sibling
-         *
-         * @return {Object}
-         */
-
-        prev: function (selector) {
-            return selector ? hAzzle(this.pluckNode('previousSibling').filter(selector)) : hAzzle(this.pluckNode('previousSibling'));
-        },
-
-        prevUntil: function (until) {
-
-            var matches = [];
-
-            this.prevAll().each(function () {
-                if (hAzzle(this).is(until)) return false;
-                matches.push(this);
-            });
-
-            return hAzzle(matches);
-        },
-
-        /**
-         * Return an sequense of elements from the 'elems stack', plucked
-         * by the given numbers
-         *
-         * Example:
-         *
-         * hAzzle('p').collection([1,6, 9])
-         *
-         * Outputs elem 1,6, 9 from the stack
-         *
-         * @param {array} count
-         * @return {object}
-         *
-         */
-
-        collection: function (count) {
-
-            if (!hAzzle.isArray(count)) {
-                return [];
-            }
-
-            var holder = [],
-                i = count.length;
-            while (i--) {
-                holder.push(this.elems[count[i]]);
-            }
-
-            return hAzzle(holder) || [];
-        },
-
-        /**
-         * Reduce the set of matched elements to the first x in the set.
-         */
-
-        first: function (count) {
-
-            if ((count === null)) {
-
-                return hAzzle(this.elems[0]);
-            }
-
-            if (count < 0) {
-
-                return [];
-            }
-
-            return this.slice(0, count);
-        },
-
-        /**
-         * Reduce the set of matched elements to the last one in the set.
-         */
-
-        last: function (count) {
-            var elems = this.elems;
-
-            if ((count === null)) {
-
-                return hAzzle(elems[elems.length - 1]);
-            }
-
-            return this.slice(Math.max(elems.length - count, 0));
-        },
-
-        // Returns everything but the first entry of the array
-
-        tail: function (count) {
-            return this.slice((count === null) ? 1 : count);
-        },
-
-        contents: function () {
-            return this.map(function (elem) {
-                return elem.contentDocument || slice.call(elem.childNodes);
-            });
-        },
-
-        /**
-         * Return the element's siblings
-         * @param {String} sel
-         * @return {Object}
-         */
-
-        siblings: function (sel) {
-
-            var siblings = [];
-
-            if (!cached[sel]) {
-                this.each(function (_, elem) {
-                    hAzzle.each(slice.call((elem.parentNode || {}).childNodes), function (_, child) {
-                        if (hAzzle.isElement(child) && hAzzle.nodeType(1, child) && child !== elem) {
-                            siblings.push(child);
-                        }
-                    });
-                });
-                cached[sel] = siblings;
-            }
-
-            return hAzzle.create(cached[sel], sel);
-        }
-
-    });
-
-    hAzzle.extend(hAzzle, {
-
-        /**
-         * Walks the DOM tree using `method`, returns when an element node is found
-         *
-         * @param{Object} element
-         * @param{String} method
-         * @param{String} sel
-         * @param{Number/Null } nt
-         */
-
-        getClosestNode: function (element, method, sel) {
-            do {
-                element = element[method];
-            } while (element && ((sel && !hAzzle.matches(sel, element)) || !hAzzle.isElement(element)));
-            return element;
-        }
-    });
+hAzzle.extend(hAzzle.fn, {
 
     /**
-     * Process nextAll and prevAll
+     * Get the  element that matches the selector, beginning at the current element and progressing up through the DOM tree.
+     *
+     * @param {String} sel
+     * @return {Object}
      */
 
-    hAzzle.each({
-        'nextAll': 'next',
-        'prevAll': 'prev'
-    }, function (name, subn) {
-
-        hAzzle.fn[name] = function (sel) {
-            var els = hAzzle(),
-                el = this[subn](); // next() or prev()
-            while (el.length) {
-                if (typeof sel === 'undefined' || el.is(sel)) {
-                    els = els.add(el);
-                }
-                el = el[subn](); // next() or prev()
+    closest: function (sel, ctx) {
+        return this.map(function (elem) {
+            if (hAzzle.nodeType(1, elem) && elem !== ctx && !hAzzle.isDocument(elem) && hAzzle.matches(elem, typeof sel === 'object' ? hAzzle(sel) : sel)) {
+                return elem;
             }
-            return els;
-        };
-    });
+            do {
+                elem = elem['parentNode'];
+            } while (elem && ((sel && !hAzzle.matches(sel, elem)) || !hAzzle.isElement(elem)));
+            return elem;
+        });
+    },
+
+    /** Determine the position of an element within the matched set of elements
+     *
+     * @param {string} elem
+     * @param {return} Object
+     */
+
+    index: function (elem) {
+        return elem ? this.indexOf(hAzzle(elem)[0]) : this.parent().children().indexOf(this[0]) || -1;
+    },
+
+    /**
+     *  Pick elements by tagNames from the "elems stack"
+     *
+     * @param {string} tag
+     * @return {Object}
+     */
+    tags: function (tag) {
+        return this.map(function (els) {
+            if (els.tagName.toLowerCase() === tag && hAzzle.nodeType(1, els)) {
+                return els;
+            }
+        });
+    },
+
+    /**
+     * Adds one element to the set of matched elements.
+     *
+     * @param {String} sel
+     * @param {String} ctx
+     * @return {Object}
+     */
+
+    add: function (sel, ctx) {
+        return this.concat(hAzzle(sel, ctx).elems);
+    },
+
+    /**
+     * Reduce the set of matched elements to those that have a descendant that matches the selector or DOM element.
+     */
+    has: function (target) {
+
+        var targets = hAzzle(target, this),
+            i = 0,
+            l = targets.length;
+
+        return this.filter(function () {
+            for (; i < l; i++) {
+                if (hAzzle.contains(this, targets[i])) {
+                    return true;
+                }
+            }
+        });
+    },
+
+    /**
+     * Remove elements from the set of matched elements.
+     *
+     * @param {String} sel
+     * @return {Object}
+     *
+     */
+
+    not: function (sel) {
+        return this.filter(sel, true);
+    },
+
+    /**
+     * Check if the first element in the element collection matches the selector
+     *
+     * @param {String|Object} sel
+     * @return {Boolean}
+     */
+
+    is: function (sel) {
+
+
+
+
+        return !!sel && (
+            /^[\x20\t\r\n\f]*[>+~]|:(even|odd|eq|gt|lt|nth|first|last)(?:\([\x20\t\r\n\f]*((?:-\d)?\d*)[\x20\t\r\n\f]*\)|)(?=[^-]|$)/i.test(sel) ?
+            hAzzle(sel).index(this[0]) >= 0 :
+            this.filter(sel).length > 0);
+    },
+
+    /**
+     * Get immediate parents of each element in the collection.
+     * If CSS selector is given, filter results to include only ones matching the selector.
+     *
+     * @param {String} sel
+     * @return {Object}
+     */
+
+    parent: function (sel) {
+        return hAzzle.create(this.pluck('parentNode', /* NodeType 11 */ 11), sel);
+    },
+    /**
+     * Return an sequense of elements from the 'elems stack', plucked
+     * by the given numbers
+     *
+     * Example:
+     *
+     * hAzzle('p').collection([1,6, 9])
+     *
+     * Outputs elem 1,6, 9 from the stack
+     *
+     * @param {array} count
+     * @return {object}
+     *
+     */
+
+    collection: function (count) {
+
+        if (!hAzzle.isArray(count)) {
+            return [];
+        }
+
+        var holder = [],
+            i = count.length;
+        while (i--) {
+            holder.push(this.elems[count[i]]);
+        }
+
+        return hAzzle(holder) || [];
+    },
+
+    /**
+     * Reduce the set of matched elements to the first x in the set.
+     */
+
+    first: function (count) {
+
+        if (count) {
+
+            return this.slice(0, count);
+        }
+
+        if (count < 0) {
+
+            return [];
+        }
+        return hAzzle(this.elems[0]);
+
+    },
+
+    /**
+     * Reduce the set of matched elements to the last one in the set.
+     */
+
+    last: function (count) {
+        var elems = this.elems;
+
+        if (count) {
+            return this.slice(Math.max(elems.length - count, 0));
+        }
+        return hAzzle(elems[elems.length - 1]);
+    },
+
+    // Returns everything but the first entry of the array
+
+    tail: function (count) {
+        return this.slice((count === null) ? 1 : count);
+    },
+
+    contents: function () {
+        return this.map(function (elem) {
+            return elem.contentDocument || slice.call(elem.childNodes);
+        });
+    },
+
+    /**
+     * Return the element's siblings
+     * @param {String} sel
+     * @return {Object}
+     */
+
+    siblings: function (sel) {
+
+        var siblings = [];
+
+        if (!cached[sel]) {
+            this.each(function (_, elem) {
+                hAzzle.each(slice.call((elem.parentNode || {}).childNodes), function (_, child) {
+                    if (hAzzle.isElement(child) && hAzzle.nodeType(1, child) && child !== elem) {
+                        siblings.push(child);
+                    }
+                });
+            });
+            cached[sel] = siblings;
+        }
+
+        return hAzzle.create(cached[sel], sel);
+    }
+
+});
+
+hAzzle.extend(hAzzle, {
+
+    /**
+     * Walks the DOM tree using `method`, returns when an element node is found
+     *
+     * @param{Object} element
+     * @param{String} method
+     * @param{String} sel
+     * @param{Number/Null } nt
+     */
+
+    getClosestNode: function (element, method, sel) {
+        do {
+            element = element[method];
+        } while (element && ((sel && !hAzzle.matches(sel, element)) || !hAzzle.isElement(element)));
+        return element;
+    },
+
+    dir: function (elem, dir, until) {
+        var matched = [],
+            truncate = until !== undefined;
+
+        while ((elem = elem[dir]) && elem.nodeType !== 9) {
+            if (elem.nodeType === 1) {
+                if (truncate && hAzzle(elem).is(hAzzle(until))) {
+                    break;
+                }
+                matched.push(elem);
+            }
+        }
+        return matched;
+    },
+
+    sibling: function (n, elem) {
+        var matched = [];
+
+        for (; n; n = n.nextSibling) {
+            if (n.nodeType === 1 && n !== elem) {
+                matched.push(n);
+            }
+        }
+
+        return matched;
+    }
+});
+
+function sibling(cur, dir) {
+    while ((cur = cur[dir]) && cur.nodeType !== 1) {}
+    return cur;
+}
+
+hAzzle.each({
+    parents: function (elem) {
+        return hAzzle.dir(elem, "parentNode");
+    },
+    parentsUntil: function (elem, i, until) {
+        return hAzzle.dir(elem, "parentNode", until);
+    },
+    next: function (elem) {
+        return sibling(elem, "nextSibling");
+    },
+    nextUntil: function (elem, i, until) {
+
+        return hAzzle.dir(elem, "nextSibling", until);
+    },
+    nextAll: function (elem) {
+        return hAzzle.dir(elem, "nextSibling");
+    },
+    prev: function (elem) {
+        return sibling(elem, "previousSibling");
+    },
+    prevAll: function (elem) {
+        return hAzzle.dir(elem, "previousSibling");
+    },
+    prevUntil: function (elem, i, until) {
+        return hAzzle.dir(elem, "previousSibling", until);
+    },
+	
+    children: function (elem) {
+        return hAzzle.sibling(elem.firstChild);
+    }
+}, function (name, fn) {
+    hAzzle.fn[name] = function (until, selector) {
+
+        var matched = hAzzle.map(this, fn, until);
+
+        if (name.slice(-5) !== "Until") {
+            selector = until;
+        }
+
+        if (selector && typeof selector === "string") {
+            matched = hAzzle.filter(selector, matched);
+        }
+
+        if (this.length > 1) {
+            // Remove duplicates
+            if (!guaranteedUnique[name]) {
+                hAzzle.unique(matched);
+            }
+
+            // Reverse order for parents* and prev-derivatives
+            if (/^(?:parents|prev(?:Until|All))/.test(name)) {
+                matched.reverse();
+            }
+        }
+        return $(matched);
+    };
+});
