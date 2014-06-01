8d9d313f4e682739aef86a567fdcda4559055e0d
KFlash <kflash123@gmail.com>
2014-06-01 13:34:20 +0800

    Started my work with CSS animation

--- modules/fx.js
+++ modules/fx.js
@@ -1,51 +1,69 @@
 /**
- * hAzzle CSS animation engine hAzzle Animation Core engine ( hACE )
- *
- * Note! Need hACE!
- *
+ * hAzzle CSS animation engine ( hCAE )
+ */
+hAzzle.extend({
+
+    /**
+     * Animate CSS nodes
+     *
+     * @param {Object} options
+     * @return {hAzzle}
+     */
+
+    animate: function (options, speed, easing, callback) {
+
+        var el,
+            anim,
+
+            // Check if the 'queue' are activated on the system
+
+            queue = typeof hAzzle.Core['queue'] !== 'undefined';
+
+        /**
+         * If the "queue" plugin are used, and length are more then 1, we
+         * run the queue system. Else we run normal 'each'
+         */
+
+        return this[typeof queue && this.length > 1 ? 'queue' : 'each'](function () {
+
+            el = this;
+
+            anim = new hAzzle.hACE()
+                .from(options.start)
+                .to(options.to)
+                .duration(options.duration)
+                .ease(hAzzle.easing[options.easing])
+                .step(function (val) {
+                    hAzzle(el).css('opacity', val);
+                })
+
+            if (!queue) {
+
+                anim.start();
+
+            }
+
+
+        });
+
+    },
+
+
+
+})
+
+/**
+ * FadeIn and FadeOut
  */
 
-
-hAzzle.extend({
-	
-	fadeIn: function(){
-		
-	return this.each(function(el) {
-		
-		
-		new hAzzle.hACE()
-                    .from(0)
-					.to(1)
-					.duration(1400)
-					.ease(hAzzle.easing.easeOutBounce)
-					.step(function(val) {
-						hAzzle(el).css('opacity', val);
-					})
-					.start();
-		
-		
-	});
-	},
-	
-	fadeOut: function(){
-	return this.each(function(el) {
-		
-
-		new hAzzle.hACE()
-                    .from(1)
-					.to(0)
-					.duration(1400)
-					.ease(hAzzle.easing.easeOutBounce)
-					.step(function(val) {
-
-						hAzzle(el).css('opacity', val);
-					})
-					.start();
-		
-		
-	});
-	
-	}
-	
-	
-	}) 
+hAzzle.each(['fadeIn', 'fadeOut'], function (name) {
+    hAzzle.Core[name] = function (speed, callback, easing) {
+        return this.animate({
+            start: name === 'fadeIn' ? 0 : 1,
+            to: name === 'fadeIn' ? 1 : 0,
+            duration: speed,
+            callback: callback,
+            easing: easing
+        });
+    }
+});
