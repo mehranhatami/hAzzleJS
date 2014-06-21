8ee44c73f0a1165d61608777974ce649000cd482
KFlash <kflash123@gmail.com>
2014-06-21 21:57:45 +0800

    Cleanups in the new shim

--- modules/shims/setImmediate.js
+++ modules/shims/setImmediate.js
@@ -66,8 +66,10 @@
     // The test against `importScripts` prevents this implementation from being installed inside a web worker,
     // where `global.postMessage` means something completely different and can't be used for this purpose.
     if (global.postMessage && !global.importScripts) {
-      var postMessageIsAsynchronous = true;
-      var oldOnMessage = global.onmessage;
+
+      var postMessageIsAsynchronous = true,
+          oldOnMessage = global.onmessage;
+
       global.onmessage = function () {
         postMessageIsAsynchronous = false;
       };
@@ -82,12 +84,12 @@
     // * https://developer.mozilla.org/en/DOM/window.postMessage
     // * http://www.whatwg.org/specs/web-apps/current-work/multipage/comms.html#crossDocumentMessages
 
-    var messagePrefix = 'setImmediate$' + Math.random() + '$';
-    var onGlobalMessage = function (event) {
-      if (event.source === global &&
-        typeof event.data === 'string' &&
-        event.data.indexOf(messagePrefix) === 0) {
-        runIfPresent(+event.data.slice(messagePrefix.length));
+    var messagePrefix = 'setImmediate$' + Math.random() + '$',
+        onGlobalMessage = function (evt) {
+      if (evt.source === global &&
+        typeof evt.data === 'string' &&
+        evt.data.indexOf(messagePrefix) === 0) {
+        runIfPresent(+evt.data.slice(messagePrefix.length));
       }
     };
 
@@ -105,9 +107,10 @@
   }
 
   function installMessageChannelImplementation() {
-    var channel = new MessageChannel();
-    channel.port1.onmessage = function (event) {
-      var handle = event.data;
+    var channel = new MessageChannel(),
+	    handle;
+    channel.port1.onmessage = function (evt) {
+       handle = evt.data;
       runIfPresent(handle);
     };
 
@@ -121,10 +124,10 @@
   function installReadyStateChangeImplementation() {
     var html = doc.documentElement;
     setImmediate = function () {
-      var handle = addFromSetImmediateArguments(arguments);
+      var handle = addFromSetImmediateArguments(arguments),
       // Create a <script> element; its readystatechange event will be fired asynchronously once it is inserted
       // into the document. Do so, thus queuing up the task. Remember to clean up once it's been called.
-      var script = doc.createElement('script');
+      script = doc.createElement('script');
       script.onreadystatechange = function () {
         runIfPresent(handle);
         script.onreadystatechange = null;
@@ -145,30 +148,19 @@
   }
 
   // If supported, we should attach to the prototype of global, since that is where setTimeout et al. live.
-  var attachTo = Object.getPrototypeOf && Object.getPrototypeOf(global);
+  var OgPO = Object.getPrototypeOf,
+     attachTo = OgPO && OgPO(global);
   attachTo = attachTo && attachTo.setTimeout ? attachTo : global;
 
   // Don't get fooled by e.g. browserify environments.
-  if ({}.toString.call(global.process) === '[object process]') {
-    // For Node.js before 0.9
-    installNextTickImplementation();
-
-  } else if (canUsePostMessage()) {
+  if (canUsePostMessage()) {
     // For non-IE10 modern browsers
     installPostMessageImplementation();
 
   } else if (global.MessageChannel) {
     // For web workers, where supported
     installMessageChannelImplementation();
-
-  } else if (doc && 'onreadystatechange' in doc.createElement('script')) {
-    // For IE 6–8
-    installReadyStateChangeImplementation();
-
-  } else {
-    // For older browsers
-    installSetTimeoutImplementation();
-  }
+  } 
 
   attachTo.setImmediate = setImmediate;
   attachTo.clearImmediate = clearImmediate;
