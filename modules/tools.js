ffb3952e5661920af4d525061b3fbf9e678a1c38
KFlash <kflash123@gmail.com>
2014-07-05 20:54:26 +0800

    cleaned up the tools.js

--- modules/tools.js
+++ modules/tools.js
@@ -1,48 +1,50 @@
 /**
  * sortOrder
  */
- 
- // Need this for CSS4 selectors
+var win = this,
+    doc = document, 
+	Jiesa = hAzzle.Jiesa;
 
-var Jiesa = hAzzle.Jiesa,
-    sO = Jiesa.sortOrder = function( a, b ) {
-		// Flag for duplicate removal
-		if ( a === b ) {
-			return 0;
-		}
+hAzzle.extend({
 
-		var compare = b.compareDocumentPosition && a.compareDocumentPosition && a.compareDocumentPosition( b );
+    sortOrder: function (a, b) {
+        // Flag for duplicate removal
+        if (a === b) {
+            return 0;
+        }
 
-		if ( compare ) {
-			// Disconnected nodes
-			if ( compare & 1 ) {
+        var compare = b.compareDocumentPosition && a.compareDocumentPosition && a.compareDocumentPosition(b);
 
-				// Choose the first element that is related to our document
-				if ( a === document || hAzzle.contains(document, a) ) {
-					return -1;
-				}
-				if ( b === document || hAzzle.contains(document, b) ) {
-					return 1;
-				}
+        if (compare) {
+            // Disconnected nodes
+            if (compare & 1) {
 
-				// Maintain original order
-				return 0;
-			}
+                // Choose the first element that is related to our document
+                if (a === doc || hAzzle.contains(doc, a)) {
+                    return -1;
+                }
+                if (b === doc || hAzzle.contains(doc, b)) {
+                    return 1;
+                }
 
-			return compare & 4 ? -1 : 1;
-		}
+                // Maintain original order
+                return 0;
+            }
 
-		// Not directly comparable, sort on existence of method
-		return a.compareDocumentPosition ? -1 : 1;
-	};
+            return compare & 4 ? -1 : 1;
+        }
 
-/* =========================== GLOBALE JIESA VARS ========================== */
+        // Not directly comparable, sort on existence of method
+        return a.compareDocumentPosition ? -1 : 1;
+    },
 
-// CSS escapes http://www.w3.org/TR/CSS21/syndata.html#escaped-characters
+    /* =========================== GLOBALE JIESA VARS ========================== */
 
-   Jiesa.whitespace = "[\\x20\\t\\r\\n\\f]";
-   Jiesa.runescape = new RegExp("\\\\([\\da-f]{1,6}" + Jiesa.whitespace + "?|(" + Jiesa.whitespace + ")|.)", "ig");
-   Jiesa.funescape = function (_, escaped, escapedWhitespace) {
+    // CSS escapes http://www.w3.org/TR/CSS21/syndata.html#escaped-characters
+
+    whitespace: "[\\x20\\t\\r\\n\\f]",
+    runescape: new RegExp("\\\\([\\da-f]{1,6}" + Jiesa.whitespace + "?|(" + Jiesa.whitespace + ")|.)", "ig"),
+    funescape: function (_, escaped, escapedWhitespace) {
         var high = "0x" + escaped - 0x10000;
         // NaN means non-codepoint
         // Support: Firefox<24
@@ -54,73 +56,75 @@ var Jiesa = hAzzle.Jiesa,
             String.fromCharCode(high + 0x10000) :
             // Supplemental Plane codepoint (surrogate pair)
             String.fromCharCode(high >> 10 | 0xD800, high & 0x3FF | 0xDC00);
-    };
+    },
 
 
-/* =========================== VARIOUS SHIMS ========================== */
-/**
- * Find next element sibiling.
- *
- * @param {Object} el
- *
- * @return {Object}
- */
 
-Jiesa.nextElementSibling = function(el) {
-    if (el.nextElementSibling) {
-        return el.nextElementSibling;
-    } else {
-        while (el = el.nextSibling) {
-            if (el.nodeType !== 1) return el;
+    /* =========================== VARIOUS SHIMS ========================== */
+    /**
+     * Find next element sibiling.
+     *
+     * @param {Object} el
+     *
+     * @return {Object}
+     */
+
+    nextElementSibling: function (el) {
+        if (el.nextElementSibling) {
+            return el.nextElementSibling;
+        } else {
+            while (el = el.nextSibling) {
+                if (el.nodeType !== 1) return el;
+            }
         }
-    }
-}
+    },
 
 
-/**
- * Find previous element sibling.
- *
- * @param {Object} el
- *
- * @return {Object}
- */
+    /**
+     * Find previous element sibling.
+     *
+     * @param {Object} el
+     *
+     * @return {Object}
+     */
 
-Jiesa.previousElementSibling = function(el) {
-    if (el.previousElementSibling) {
-        return el.previousElementSibling;
-    } else {
-        while (el = el.previousSibling) {
-            if (el.nodeType === 1) return el;
+    previousElementSibling: function (el) {
+        if (el.previousElementSibling) {
+            return el.previousElementSibling;
+        } else {
+            while (el = el.previousSibling) {
+                if (el.nodeType === 1) return el;
+            }
         }
+    },
+
+    firstElementChild: function (el) {
+        var child = el.firstElementChild;
+        if (!child) {
+            child = el.firstChild;
+            while (child && child.nodeType !== 1)
+                child = child.nextSibling;
+        }
+        return child;
+    },
+
+    lastElementChild: function (el) {
+        var child = el.lastElementChild;
+        if (!child) {
+            child = el.lastChild;
+            while (child && child.nodeType !== 1)
+                child = child.previousSibling;
+        }
+        return child;
+    },
+
+    next: function (el) {
+        while ((el = el.nextSibling) && el.nodeType !== 1);
+        return el;
+    },
+
+    prev: function (el) {
+        while ((el = el.previousSibling) && el.nodeType !== 1);
+        return el;
     }
-}
-
-Jiesa.firstElementChild = function(el) {
-    var child = el.firstElementChild;
-    if (!child) {
-        child = el.firstChild;
-        while (child && child.nodeType !== 1)
-            child = child.nextSibling;
-    }
-    return child;
-}
-
-Jiesa.lastElementChild = function(el) {
-    var child = el.lastElementChild;
-    if (!child) {
-        child = el.lastChild;
-        while (child && child.nodeType !== 1)
-            child = child.previousSibling;
-    }
-    return child;
-}
-
-Jiesa.next = function(el) {
-    while ((el = el.nextSibling) && el.nodeType !== 1);
-    return el;
-}
-
-Jiesa.prev = function(el) {
-    while ((el = el.previousSibling) && el.nodeType !== 1);
-    return el;
-}
+});
