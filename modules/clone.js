dc54bd5a131eee637dab36b87a9a4d76c5783c84
KFlash <kflash123@gmail.com>
2014-07-07 21:15:07 +0800

    fixed the clone.js module

--- modules/clone.js
+++ modules/clone.js
@@ -1,76 +1,93 @@
 /*!
- * Clone
+ * Clone DOM nodes
+ *
+ * Supports:
+ *
+ * - data cloning
+ * - event cloning
  */
 var rcheckableType = /^(?:checkbox|radio)$/i;
 
+/**
+ * Helper function for cloning select and textareas e.g.
+ */
+
 function fixInput(src, dest) {
     var nodeName = dest.nodeName.toLowerCase();
-    if ("input" === nodeName && rcheckableType.test(src.type)) {
+    // checkbox / radio
+    if (nodeName === 'input' && rcheckableType.test(src.type)) {
         dest.checked = src.checked;
-    } else if ("input" === nodeName || "textarea" === nodeName) {
+        // textarea
+    } else if (nodeName === 'input' || 'textarea' === nodeName) {
         dest.defaultValue = src.defaultValue;
     }
 }
 
-hAzzle.extend({
+hAzzle.cloneNode = function (el, deep) {
 
-    cloneNode: function (el, deep) {
+    if (!el) {
 
-        if (!el) {
+        return;
+    }
 
-            return;
-        }
+    var c = el.cloneNode(deep || true),
+        cloneElems, elElems;
 
-        var c = el.cloneNode(deep || true),
-            cloneElems, elElems;
+    hAzzle(c).cloneEvents(el);
 
-        hAzzle(c).cloneEvents(el);
+    // Copy the events from the original to the clone
+    // We could have used Jiesa.parse() here,
+    // but we don't know if native QSA are used
+    // or not. 
 
-        // clone events from every child node
-        cloneElems = hAzzle.select("*", c);
-        elElems = hAzzle.select("*", el);
+    cloneElems = hAzzle.select('*', c);
+    elElems = hAzzle.select('*', el);
 
-        var i = 0,
-            len = elElems.length;
+    var i = 0,
+        len = elElems.length;
 
-        // Copy Events
+    // Copy Events
+
+    for (; i < len; i++) {
+
+        hAzzle(cloneElems[i]).cloneEvents(elElems[i]);
+    }
+    // hAzzle.documentIsHTML are not helping us here
+    // we need to check directly with current DOM node
+    // This also fixes the IE cloning issues
+
+    if (!hAzzle.features.noCloneChecked && el.nodeType === 1 || el.nodeType === 11 && hAzzle.hAzzle.isXML(el)) {
 
         for (; i < len; i++) {
-            hAzzle(cloneElems[i]).cloneEvents(elElems[i]);
+
+            fixInput(elElems[i], cloneElems[i]);
         }
 
-        if (!hAzzle.features.noCloneChecked && el.nodeType === 1 || el.nodeType === 11 && hAzzle.isXML(el)) {
-            for (; i < len; i++) {
-                fixInput(elElems[i], cloneElems[i]);
-            }
-        } else {
+    } else {
 
-            // Okey, Mehran. We have cloned. Let us copy over the textarea data
+        // Clone job done! Copy radio, checkbox and textarea if any...
 
-            var cloneTextareas = hAzzle.select("textarea", c),
-                elTextareas = hAzzle.select("textarea", el),
-                a = 0,
-                b = elTextareas.length;
+        var cloneTextareas = hAzzle.select('textarea', c),
+            elTextareas = hAzzle.select('textarea', el),
+            a = 0,
+            b = elTextareas.length;
 
-            // Copy over the textarea data	 
+        // Copy over the cloned elements data
 
-            for (; a < b; ++a) {
-                hAzzle(cloneTextareas[b]).val(hAzzle(elTextareas[b]).val());
-            }
+        for (; a < b; ++a) {
+
+            hAzzle(cloneTextareas[b]).val(hAzzle(elTextareas[b]).val());
         }
-
-        // Return the cloned set
-
-        return c;
     }
-}, hAzzle);
 
+    // Return the cloned set
 
-hAzzle.extend({
-    clone: function (deep) {
-        var self = this;
-        return self[0] ? self.twist(function (el) {
-            return hAzzle.cloneNode(el, deep);
-        }) : self;
-    }
-});
+    return c;
+};
+
+// Extend the hAzzle Core
+hAzzle.Core['clone'] = function (deep) {
+    return this.twist(function (el) {
+        return hAzzle.cloneNode(el, deep);
+    });
+};
