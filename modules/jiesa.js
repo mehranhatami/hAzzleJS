14001943dde3e0b58089cfe46e5dfbeb9e69e6ef
KFlash <kflash123@gmail.com>
2014-07-08 14:19:54 +0800

    jiesa fix again!!!

--- modules/jiesa.js
+++ modules/jiesa.js
@@ -21,7 +21,7 @@ var win = this,
 
     push = Array.prototype.push,
 
-    // Various regEx we will need
+    // Various regEx
 
     sibling = /[+~]/,
 
@@ -42,10 +42,7 @@ hAzzle.extend({
         // Feature detect if the browser supports QSA
 
         'api-QSA': !!doc.querySelectorAll,
-
-    },
-
-    cache: {}
+    }
 
 }, Jiesa);
 
@@ -55,8 +52,10 @@ hAzzle.extend({
 
 Jiesa.has['bug-QSA'] = Jiesa.has['api-QSA'] ? hAzzle.assert(function (div) {
     div.innerHTML = "<p class='QsA'>Jiesa</p>";
-    return div.querySelectorAll(".QsA").length === 0;
-}) : false
+    return div.querySelectorAll(".QsA").length === 0 ? false :
+        // Check for broken :checked pseudo in Webkit/Opera
+        !div.querySelectorAll(":checked").length ? false : true;
+}) : false;
 
 /**
  * Check if getElementsByTagName ("*") returns only elements
@@ -81,7 +80,6 @@ Jiesa.has["bug-GEBI"] = hAzzle.assert(function (div) {
  * @param {Function} fn
  */
 
-
 // Extend Jiesa
 
 hAzzle.extend({
@@ -90,7 +88,7 @@ hAzzle.extend({
      * Jiesa selector engine
      *
      * @param {String} selector
-     * @param {String/Object/Array}	context
+     * @param {String/Object/Array} context
      * @param {Array} results
      * @param {Boolean} single
      * @return {hAzzle}
@@ -161,9 +159,8 @@ hAzzle.extend({
             }
 
             // If querySelectorAll are activated, and not buggy,
-            // existing, and not XML doc,
-            // we can use QSA. If not, we fallback
-            // to our internal selector engine
+            // existing, and no XML doc - use QSA. If not, fallback
+            // to ther internal selector engine 
 
             if (Jiesa.useNative && Jiesa.has['api-QSA'] && !Jiesa.has['bug-QSA']) {
 
