ac915ef06d27840704ec5197406b3f62c1039dbf
KFlash <kflash123@gmail.com>
2014-07-08 09:24:41 +0800

    finished off Jiesa.js

--- modules/jiesa.js
+++ modules/jiesa.js
@@ -8,10 +8,10 @@
  * - Various bug checks
  */
 var win = this,
-    Jiesa = hAzzle.Jiesa,
-    doc = win.document,
 
-    useNative = false,
+    Jiesa = hAzzle.Jiesa,
+
+    doc = win.document,
 
     expando = "hAzzle" + -(new Date()),
 
@@ -36,7 +36,6 @@ hAzzle.extend({
 }, Jiesa);
 
 // Feature / Bug detection
-// NOTE!! Need to add better check for this. See Sizzle
 
 (function () {
 
@@ -97,21 +96,22 @@ function assert(fn) {
 
 hAzzle.extend({
 
-	/**
-	 * Jiesa selector engine
-	 *
-	 * @param {String} selector
-	 * @param {String/Object/Array}	context
-	 * @param {Array} results	 
-	 * @param {Boolean} single
-	 *
-	 * 'single' are used if we want to use
-	 * querySelector and not qierySelectorAll
-	 */
+    /**
+     * Jiesa selector engine
+     *
+     * @param {String} selector
+     * @param {String/Object/Array}	context
+     * @param {Array} results
+     * @param {Boolean} single
+     *
+     * 'single' are used if we want to use
+     * querySelector and not querySelectorAll
+     */
 
     find: function (selector, context, results, /* INTERNAL */ single) {
 
-        var match, elem, m, nodeType;
+        var elem, quickMatch = rquickExpr.exec(selector),
+            m, nodeType;
 
         // Set correct document
 
@@ -123,20 +123,15 @@ hAzzle.extend({
         context = context || doc;
         results = results || [];
 
-        if (!selector || typeof selector !== "string") {
-
-            return results;
-        }
-
         if ((nodeType = context.nodeType) !== 1 && nodeType !== 9) {
             return [];
         }
 
-        if (!hAzzle.documentIsHTML) {
+        if (hAzzle.documentIsHTML) {
 
-            // Shortcuts
-            if ((match = rquickExpr.exec(selector))) {
-                if ((m = match[1])) {
+            if (quickMatch) {
+
+                if ((m = quickMatch[1])) {
                     if (nodeType === 9) {
                         elem = context.getElementById(m);
                         if (elem && elem.parentNode) {
@@ -156,56 +151,67 @@ hAzzle.extend({
                         }
                     }
 
-                } else if (match[2]) {
+                    // Tag
+
+                } else if (quickMatch[2]) {
                     push.apply(results, context.getElementsByTagName(selector));
                     return results;
-                } else if ((m = match[3]) && context.getElementsByClassName) {
-                    push.apply(results, context.getElementsByClassName(m));
+
+                    // Class
+
+                } else if (context.getElementsByClassName) {
+                    push.apply(results, context.getElementsByClassName(quickMatch[3]));
                     return results;
                 }
             }
-        }
 
-        // If querySelectorAll are activated, and not buggy,
-        //	existing, and not XML doc,
-        // we can use QSA. If not, we fallback
-        // to our internal selector engine
+            // If querySelectorAll are activated, and not buggy,
+            // existing, and not XML doc,
+            // we can use QSA. If not, we fallback
+            // to our internal selector engine
 
-        if (Jiesa.useNative && Jiesa.has['api-QSA'] &&
-            !Jiesa.has['bug-QSA']) {
+            if (Jiesa.useNative && Jiesa.has['api-QSA'] && !Jiesa.has['bug-QSA']) {
 
-            var old = true,
-                nid = expando;
+                var old = true,
+                    nid = expando;
 
-            if (context !== doc) {
+                if (context !== doc) {
 
-                if ((old = context.getAttribute('id'))) {
+                    // Thanks to Andrew Dupont for the technique
 
-                    nid = old.replace(rescape, '\\$&');
+                    old = context.getAttribute('id');
 
-                } else {
+                    if (old) {
 
-                    context.setAttribute('id', nid);
+                        nid = old.replace(rescape, '\\$&');
+
+                    } else {
+
+                        context.setAttribute('id', nid);
+                    }
+
+                    nid = "[id='" + nid + "'] ";
+
+                    context = rsibling.test(selector) ? context.parentNode : context;
+                    selector = nid + selector.split(',').join(',' + nid);
                 }
 
-                nid = "[id='" + nid + "'] ";
+                try {
 
-                context = rsibling.test(selector) ? context.parentNode : context;
-                selector = nid + selector.split(',').join(',' + nid);
-            }
+                    // Use 'querySelector' if single === true, otherwise use 'querySelectorAll'
 
-            try {
+                    push.apply(results, context[single ? 'querySelector' : 'querySelectorAll'](selector));
+                    return results;
 
-                push.apply(results, context[single ? 'querySelector' : 'querySelectorAll'](selector));
-                return results;
+                } finally {
 
-            } catch (qsaError) {} finally {
-                if (!old) {
-                    context.removeAttribute("id");
+                    if (!old) {
+
+                        context.removeAttribute("id");
+                    }
                 }
             }
         }
-
         // Run the parser
 
         return hAzzle.merge(results, Jiesa.parse(selector.replace(rtrim, "$1"), context));
