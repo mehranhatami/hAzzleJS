1b0de2df426de45abd4a8b33eba72d0558fff02a
KFlash <kflash123@gmail.com>
2014-07-11 16:14:03 +0800

    units moved to separate module

--- modules/css.js
+++ modules/css.js
@@ -2,12 +2,8 @@
  * CSS
  */
 var win = this,
-    doc = win.document,
     docElem = hAzzle.docElem,
     numbs = /^([+-])=([+-]?(?:\d*\.|)\d+(?:[eE][+-]?\d+|))(.*)/i,
-    lrmp = /^(left$|right$|margin|padding)/,
-    reaf = /^(relative|absolute|fixed)$/,
-    topbot = /^(top|bottom)$/,
 
     cssCore = {
 
@@ -322,92 +318,7 @@ hAzzle.extend({
         return val;
     },
 
-    /**
-     * Converts one unit to another
-     *
-     * @param {Object} target
-     * @param {String} prop
-     * @param {String} returnUnit
-     *
-     */
-
-    units: function (px, unit, elem, prop) {
-
-        var val, num;
-
-        if (unit === '' || unit === 'px') {
-
-            return px; // Return if already 'px' or blank
-        }
-
-        if (unit === '%') {
-
-            if (lrmp.test(prop)) {
-                prop = 'width';
-
-            } else if (topbot.test(prop)) {
-
-                prop = 'height';
-            }
-
-            if (reaf.test(hAzzle.css(elem, 'position'))) {
-
-                elem = elem.offsetParent;
-
-            } else {
-
-                elem = elem.parentNode;
-            }
-
-            if (elem) {
-
-                val = hAzzle.css(elem, prop);
-                num = num = parseFloat(val);
-
-                prop = hAzzle.isNumeric(num) ? num || 0 : val;
-
-                if (prop !== 0) {
-
-                    return px / prop * 100;
-                }
-            }
-            return 0;
-        }
-
-        if (unit === 'em') {
-
-
-            val = hAzzle.css(elem, 'fontSize');
-            num = parseFloat(val);
-
-            prop = hAzzle.isNumeric(num) ? num || 0 : val;
-
-            return px / prop;
-        }
-
-        if (hAzzle.units.unity === undefined) {
-
-            var units = hAzzle.units.unity = {},
-                div = doc.createElement('div');
-
-            div.style.width = '100cm';
-
-            doc.body.appendChild(div);
-            units.mm = div.offsetWidth / 1000;
-            doc.body.removeChild(div);
-            units.cm = units.mm * 10;
-            units.in = units.cm * 2.54;
-            units.pt = units.in * 1 / 72;
-            units.pc = units.pt * 12;
-        }
-
-        // If the unity specified is not recognized we return the value.
-
-        unit = hAzzle.units.unity[unit];
-
-        return unit ? px / unit : px;
-
-    }
+   
 
 }, hAzzle);
 
---
+++ modules/units.js
@@ -0,0 +1,92 @@
+var win = this,
+    doc = win.document,
+    lrmp = /^(left$|right$|margin|padding)/,
+    reaf = /^(relative|absolute|fixed)$/,
+    topbot = /^(top|bottom)$/;
+
+/**
+ * Converts one unit to another
+ *
+ * @param {Object} target
+ *  @param {String} prop
+ * @param {String} returnUnit
+ *
+ */
+
+hAzzle.units = function (px, unit, elem, prop) {
+
+    var val, num;
+
+    if (unit === '' || unit === 'px') {
+
+        return px; // Return if already 'px' or blank
+    }
+
+    if (unit === '%') {
+
+        if (lrmp.test(prop)) {
+            prop = 'width';
+
+        } else if (topbot.test(prop)) {
+
+            prop = 'height';
+        }
+
+        if (reaf.test(hAzzle.css(elem, 'position'))) {
+
+            elem = elem.offsetParent;
+
+        } else {
+
+            elem = elem.parentNode;
+        }
+
+        if (elem) {
+
+            val = hAzzle.css(elem, prop);
+            num = num = parseFloat(val);
+
+            prop = hAzzle.isNumeric(num) ? num || 0 : val;
+
+            if (prop !== 0) {
+
+                return px / prop * 100;
+            }
+        }
+        return 0;
+    }
+
+    if (unit === 'em') {
+
+
+        val = hAzzle.css(elem, 'fontSize');
+        num = parseFloat(val);
+
+        prop = hAzzle.isNumeric(num) ? num || 0 : val;
+
+        return px / prop;
+    }
+
+    if (hAzzle.units.unity === undefined) {
+
+        var units = hAzzle.units.unity = {},
+            div = doc.createElement('div');
+
+        div.style.width = '100cm';
+
+        doc.body.appendChild(div);
+        units.mm = div.offsetWidth / 1000;
+        doc.body.removeChild(div);
+        units.cm = units.mm * 10;
+        units.in = units.cm * 2.54;
+        units.pt = units.in * 1 / 72;
+        units.pc = units.pt * 12;
+    }
+
+    // If the unity specified is not recognized we return the value.
+
+    unit = hAzzle.units.unity[unit];
+
+    return unit ? px / unit : px;
+
+}
