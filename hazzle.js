a15010541cae16b7103d92d7f3a0c12c66144247
KFlash <kflash123@gmail.com>
2014-07-14 00:16:56 +0800

    a

--- hazzle.js
+++ hazzle.js
@@ -1,54 +1,53 @@
 /*! hazzle v0.0.0 - MIT license */
-
 (function (win) {
-  function moduleDefinition() {
-    return win.hAzzle;
-  }
-  var path = 'modules/',
-    hazzleModules = [
-	  	 path + 'hazzle.js',
-         path + 'ntapi.js',
-		 path + 'types.js',        
-		 path + 'ready.js',        
-		 path + 'shims/pnow.js',
-		 path + 'document.js',
-         path + 'core.js',
-         path + 'cl3.js',
-         path + 'cl4.js',
-         path + 'changers.js',
-         path + 'compile.js',
-         path + 'jiesa.js',
-		 path + 'funcs.js',
-		 path + 'fx.js',
-		 path + 'data.js',
-         path + 'shims/classlist.js',
-		 path + 'classes.js',
-		 path + 'html.js',
-		 path + 'manipulation.js',
-   		 path + 'attributes.js',
-		 path + 'removeable.js',
-		 path + 'units.js',
-         path + 'css.js',
-         path + 'position.js',
-         path + 'offset.js',
-		 path + 'showhide.js',
-		 path + 'detection.js',
-		 path + 'events.js',
-		 path + 'eventhooks.js',
-		 path + 'ajax.js',
-		 path + 'clone.js',
-		 path + 'parsing.js',
-		 path + 'localestorage.js'
-	  
-    ];
-  if (typeof exports === 'object') {
-    // node export
-    module.exports = moduleDefinition();
-  } else if (typeof define === 'function' && define.amd) {
-    // amd anonymous module registration
-    define(hazzleModules, moduleDefinition);
-  } else {
-    // browser global
-    win.hAzzle = moduleDefinition();
-  }
+    function moduleDefinition() {
+        return win.hAzzle;
+    }
+    var path = 'modules/',
+        hazzleModules = [
+            path + 'hazzle.js',
+            path + 'ntapi.js',
+            path + 'types.js',
+            path + 'ready.js',
+            path + 'shims/pnow.js',
+            path + 'document.js',
+            path + 'core.js',
+            path + 'cl3.js',
+            path + 'cl4.js',
+            path + 'changers.js',
+            path + 'compile.js',
+            path + 'jiesa.js',
+            path + 'funcs.js',
+            path + 'fx.js',
+            path + 'data.js',
+            path + 'shims/classlist.js',
+            path + 'classes.js',
+            path + 'html.js',
+            path + 'manipulation.js',
+            path + 'attributes.js',
+            path + 'removeable.js',
+            path + 'units.js',
+            path + 'css.js',
+            path + 'position.js',
+            path + 'offset.js',
+            path + 'showhide.js',
+            path + 'detection.js',
+            path + 'events.js',
+            path + 'eventhooks.js',
+            path + 'ajax.js',
+            path + 'clone.js',
+            path + 'parsing.js',
+            path + 'localestorage.js'
+
+        ];
+    if (typeof exports === 'object') {
+        // node export
+        module.exports = moduleDefinition();
+    } else if (typeof define === 'function' && define.amd) {
+        // amd anonymous module registration
+        define(hazzleModules, moduleDefinition);
+    } else {
+        // browser global
+        win.hAzzle = moduleDefinition();
+    }
 }(this));
