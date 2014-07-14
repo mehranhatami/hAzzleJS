63da87769913bcb9c55333e6284da4a1a618ede7
KFlash <kflash123@gmail.com>
2014-07-14 22:02:43 +0800

    try again

--- modules/events.js
+++ modules/events.js
@@ -1134,15 +1134,3 @@ hAzzle.extend({
         });
     }
 });
-
-hAzzle.each(('blur focus focusin focusout load resize scroll unload click dblclick ' +
-    'mousedown mouseup mousemove mouseover mouseout mouseenter mouseleave ' +
-    'change select submit keydown keypress keyup error contextmenu').split(' '), function (evt) {
-
-
-    hAzzle.Core[evt] = function (delegate, fn) {
-        return arguments.length > 0 ?
-            this.on(evt, delegate, fn) :
-            this.trigger(evt);
-    };
-});
