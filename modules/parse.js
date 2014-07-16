41a76b05919d3c998d77c19bf6c1517c154e46ea
KFlash <kflash123@gmail.com>
2014-07-16 23:13:06 +0800

    refactoring again

--- modules/parse.js
+++ modules/parse.js
@@ -1,305 +1,164 @@
-/** 
- * Generate HTML from CSS selectors
- 
- A FEW EXAMPLES:
- 
-  #ID
-  ---
-  
-  hAzzle.html('div#hello');  
+    var _slice = Array.prototype.slice,
 
-  RESULT: 
-  
-  <div id="hello"> <div>
-
-  #CLASS
-  ------
-  
-  hAzzle.html('div.hello');  
-
-  RESULT: 
-  
-  <div class="hello"> <div>
-
-  #ID AND CLASS
-  ------
-  
-  hAzzle.html('div#hello.test');  
-
-  RESULT: 
-  
-  <div id="hello" class="test"> <div>
-
-  DIV AND A-TAG
-  -------------
-
- hAzzle.html('div a');
- 
-  RESULT:  
-  
-  <div>
-    <a></a>
-</div>
+        expr = {
+            operators: /[>+]/g,
+            multiplier: /\*(\d+)$/,
+            id: /#[\w-$]+/g,
+            tagname: /^\w+/,
+            classname: /\.[\w-$]+/g,
+            attributes: /\[([^\]]+)\]/g,
+            values: /([\w-]+)(\s*=\s*(['"]?)([^,\]]+)(\3))?/g,
+            numbering: /[$]+/g,
+            text: /\{(.+)\}/
+        };
 
 
-  HTML
-  -------------
+    hAzzle.html = function (str, data) {
 
- hAzzle.html('div[innerHTML=FOO]')
-  
-  RESULT:
-  
-  <div> FOO </div>
+        var parts = str.split(expr.operators).map(Function.prototype.call, String.prototype.trim),
+            tree = document.createDocumentFragment(),
+            parents,
+        matches;
+
+        parents = [tree];
+
+        hAzzle.each(parts, function (original) {
+
+            var part = original,
+                op = (expr.operators.exec(str) || [])[0],
+                count = 1,
+                tag,
+                id,
+                classes,
+                text,
+                index, _index, element,
+                attrs = {};
 
 
-  hAzzle.html('div span + a');
-  
-
- IMAGES
-  -------------
-
- hAzzle.html('img src="/test.png"')
-  
-  RESULT:
-  
-  <img src="/test.png"> FOO </img>
-
-
- SETTING ATTRIBUTES:
-  -------------
- hAzzle.html('input[type=checkbox][checked]');
-
-**/
-var win = this,
-    doc = win,
-
-    chunker = /((?:\((?:\([^()]+\)|[^()]+)+\)|\[(?:\[[^[\]]*\]|['"][^'"]*['"]|[^[\]'"]+)+\]|\\.|[^ >+~,(\[\\]+)+|[>+~])(\s*,\s*)?/g,
-
-    white = /^\s+$/,
-
-    trimspaces = /^\s*|\s*$/g,
-
-    matchExpr = {
-
-        ID: /#((?:[\w\u00c0-\uFFFF_-]|\\.)+)/,
-        CLASS: /\.((?:[\w\u00c0-\uFFFF_-]|\\.)+)(?![^[\]]+])/g,
-        NAME: /\[name=['"]*((?:[\w\u00c0-\uFFFF_-]|\\.)+)['"]*\]/,
-        ATTR: /\[\s*((?:[\w\u00c0-\uFFFF_-]|\\.)+)\s*(?:(\S?=)\s*(['"]*)(.*?)\3|)\s*\]/g,
-        TAG: /^((?:[\w\u00c0-\uFFFF\*_-]|\\.)+)/,
-        CLONE: /\:(\d+)(?=$|[:[])/,
-        COMBINATOR: /^[>~+]$/
-    },
-
-    cache = {},
-
-    tagTypes = ['ID', 'CLASS', 'NAME', 'ATTR'],
-
-    expr = {
-
-        attr: {
-            'for': 'htmlFor',
-            'class': 'className',
-            'html': 'innerHTML'
-        },
-
-        filter: {
-
-            'ID': function (match, node) {
-                node.id = match[1];
-            },
-            'CLASS': function (match, node) {
-                var cls = node.className.replace(white, '');
-                node.className = cls ? cls + ' ' + match[1] : match[1];
-            },
-            'NAME': function (match, node) {
-                node.name = match[1];
-            },
-            'ATTR': function (match, node) {
-
-                var attr = match[1],
-                    val = match[4] || true;
-
-                if (val === true || attr === 'innerHTML' || expr.attr.hasOwnProperty(attr)) {
-
-                    node[expr.attr[attr] || attr] = val;
-
-                } else {
-
-                    node.setAttribute(attr, val);
+            if ((matches = part.match(expr.attributes))) {
+                var matched = matches[matches.length - 1];
+                while ((matches = expr.values.exec(matched))) {
+                    attrs[matches[1]] = (matches[4] || '').replace(/['"]/g, '').trim();
                 }
-
+                part = part.replace(expr.attributes, '');
             }
-        }
+
+            // #### Multipliers
+            if ((matches = part.match(expr.multiplier))) {
+                var times = +matches[1];
+                if (times > 0) count = times;
+            }
+
+            // #### IDs
+            if ((matches = part.match(expr.id))) {
+                id = matches[matches.length - 1].substr(1);
+            }
+
+            // #### Tag names
+            if ((matches = part.match(expr.tagname))) {
+                tag = matches[0];
+            } else {
+                tag = 'div';
+            }
+
+            // #### Class names
+            if ((matches = part.match(expr.classname))) {
+                classes = matches.map(function (c) {
+                    return c.substr(1);
+                }).join(' ');
+            }
+
+            // #### Text
+            if ((matches = part.match(expr.text))) {
+                text = matches[1];
+                if (data) {
+                    text = text.replace(/\$(\w+)/g, function (m, key) {
+                        return data[key];
+                    });
+                }
+            }
+
+            // Insert `count` copies of the element per parent. If the current operator
+            // is `+` we mark the elements to remove it from `parents` in the next iteration.
+            hAzzle.each(_slice.call(parents, 0), function (parent, parentIndex) {
+                for (index = 0; index < count; index++) {
+                    // Use parentIndex if this element has a count of 1
+                    _index = count > 1 ? index : parentIndex;
+
+                    element = Element(_index, tag, id, classes, text, attrs);
+                    if (op === '+') element._sibling = true;
+
+                    parent.appendChild(element);
+                }
+            });
+
+            // If the next operator is '>' replace `parents` with their childNodes for the next iteration.
+            if (op === '>') {
+                parents = parents.reduce(function (p, c) {
+                    return p.concat(_slice.call(c.childNodes, 0).filter(function (el) {
+                        return el.nodeType === 1 && !el._sibling;
+                    }));
+                }, []);
+            }
+
+        });
+
+        var div = document.createElement('div');
+        div.appendChild(tree.cloneNode(true));
+
+        return hAzzle(div);
     };
 
 
-hAzzle.html = function (selector) {
 
-    // Remove whitespace
 
-    selector = selector.replace(trimspaces, '');
-
-    // Return if the selector are cached
-
-    if (selector in cache) {
-
-        return cache[selector].cloneNode(true).childNodes;
+    // Pads number `n` with `ln` zeroes.
+    function pad(n, ln) {
+        n = n.toString();
+        while (n.length < ln) {
+            n = '0' + n;
+        }
+        return n;
     }
 
-    var nodes = [],
-        container = [],
-        fragment = doc.createDocumentFragment(),
-        children,
-        prevChildren,
-        curSelector,
-
-        /** Keep track of how many duplicates / clones of an tag we
-         * are creating.
-         *
-         * E.g hAzzle.html('div:49')
-         *
-         * Clones will be set to 49.
-         */
-
-        Clones = 1,
-        Parts = 0,
-        isSibling = false,
-        cloneMatch,
-        m;
-
-    for (;
-        (m = chunker.exec(selector)) !== null;) {
-
-        ++Parts, nodes.push(m[1]);
+    // Replaces ocurrences of '$' with the equivalent padded index.
+    // `$$ == 01`, `$$$$ == 0001`
+    function numbered(value, n) {
+        return value.replace(expr.numbering, function (m) {
+            return pad(n + 1, m.length);
+        });
     }
 
-    // We're going in reverse
+	    // Create a DOM element.
+    function Element(index, tag, id, className, text, attrs) {
 
-    while (Parts--) {
+        var key, element = document.createElement(tag);
 
-        curSelector = nodes[Parts];
+        if (id) {
 
-        if (matchExpr.COMBINATOR.test(curSelector)) {
+            element.id = numbered(id, index);
 
-            isSibling = curSelector === '~' || curSelector === '+';
-            continue;
         }
 
-        Clones = (cloneMatch = curSelector.match(matchExpr.CLONE)) ? ~~cloneMatch[1] : 1;
+        if (className) {
 
-        prevChildren = children;
-
-
-        // Always make sure 'Clones' are a valid
-        // number. If not, set to 1
-
-        if (typeof Clones !== 'number') {
-
-            Clones = 1;
+            element.className = numbered(className, index);
         }
 
-        children = create(curSelector, Clones);
+        if (text) {
 
-        if (prevChildren) {
+            element.appendChild(document.createTextNode(text));
 
-            if (isSibling) {
-                children.appendChild(prevChildren);
-                isSibling = false;
-            } else {
-                multiAppend(children, prevChildren);
+        }
+
+        if (attrs) {
+            for (key in attrs) {
+                if (!attrs.hasOwnProperty(key)) {
+
+                    continue;
+                }
+                element.setAttribute(key, numbered(attrs[key], index));
             }
         }
+        return element;
     }
-
-    fragment.appendChild(children);
-
-    cache[selector] = fragment.cloneNode(true);
-
-    hAzzle.merge(container, fragment.childNodes);
-
-    // Remove wrapper from fragment
-
-    fragment.textContent = "";
-
-    // Clear inner HTML
-
-    fragment.innerHTML = "";
-
-    return hAzzle(container);
-};
-
-/* =========================== INTERNAL FUNCTIONS ========================== */
-
-/** 
- * Create the HTML
- *
- * @param {String} part
- * @param {Number} n
- */
-
-function create(part, n) {
-
-
-    var tag = matchExpr.TAG.exec(part),
-        node = doc.createElement(tag && tag[1] !== '*' ? tag[1] : 'div'),
-        fragment = doc.createDocumentFragment(),
-        c = tagTypes.length,
-        match, regex, callback;
-
-    while (c--) {
-
-        regex = matchExpr[tagTypes[c]];
-        callback = expr.filter[tagTypes[c]];
-
-        if (regex.global) {
-
-            while ((match = regex.exec(part)) !== null) {
-                callback(match, node);
-            }
-
-            continue;
-
-        }
-
-        if ((match = regex.exec(part))) {
-            callback(match, node);
-        }
-
-    }
-
-    while (n--) {
-		
-        fragment.appendChild(node.cloneNode(true));
-    }
-
-    return fragment;
-}
-
-
-/** 
- * Multi append more then one element
- *
- * @param {Object} parents
- * @param {Object} children
- */
-
-
-function multiAppend(parents, children) {
-
-    parents = parents.childNodes;
-
-    var i = parents.length,
-        parent;
-
-    while (i--) {
-
-        parent = parents[i];
-
-        if (parent.nodeName.toLowerCase() === 'table') {
-            /* IE requires table to have tbody */
-            parent.appendChild(parent = doc.createElement('tbody'));
-        }
-
-        parent.appendChild(children.cloneNode(true));
-    }
-}
