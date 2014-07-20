9446466e6b575ed4106ce00ec022316f41fc9c36
KFlash <kflash123@gmail.com>
2014-07-20 14:00:55 +0800

    added Mehrans fx.js (edited)

--- modules/fx.js
+++ modules/fx.js
@@ -1,110 +1,133 @@
-/**
- * Animation engine
- *
- * FX development by: Mehran Hatami
- *
- *
- */
 
-var fx = hAzzle.fx = function (elem, options, duration, callback) {
-    this.elem = elem;
-    this.options = options;
-    this.duration = duration || 400;
-    this.callback = callback;
+var now = hAzzle.pnow;
+
+var fx = function (elem, options, duration, callback) {
+  this.elem = elem;
+  this.options = options;
+  this.duration = duration || 400;
+  this.callback = callback;
 };
+
 
 fx.prototype = {
-    started: false,
+  started: false,
 
-    rafId: null,
-    animObjects: [],
-    start: function start() {
-        this.beforeStart();
-        this.animate();
-    },
+  rafId: null,
+  animObjects: [],
 
-    stop: function stop() {
-        if (hAzzle.isFunction(this.callback)) {
-            this.callback();
+  start: function () {
+
+    this.setup();
+
+    var tick = (function (thisArg) {
+      return function () {
+        thisArg.draw();
+      };
+    }(this));
+
+    var raf = (function (thisArg) {
+      return function raf() {
+        if (thisArg.rafId) {
+          hAzzle.requestFrame(raf);
+          tick();
         }
-        this.stopped = true;
-       window.cancelAnimationFrame(this.rafId);
-        this.rafId = null;
-    },
+      };
+    }(this));
 
-    animate: function animate() {
-        if (!this.stopped) {
-            this.rafId = hAzzle.requestFrame(this.animate.bind(this));
-            this.draw();
-        }
-    },
-    beforeStart: function () {
-        var i = 0,
-            props = Object.keys(this.options),
-            l = props.length,
-            prop,
-            elem = this.elem,
-            style = this.elem.style,
-            val;
-
-        this.startTime = now();
-
-        for (; i < l; i++) {
-            prop = props[i];
-            val = this.options[prop];
-
-            if (style[prop] !== undefined) {
-
-                var obj = {
-                    elem: this.elem,
-                    prop: prop,
-                    options: this.options,
-                    start: parseInt(hAzzle.css(elem, prop), 10),
-                    end: parseInt(val, 10)
-                };
-
-                obj.now = obj.start;
-
-                this.animObjects.push(obj);
-            }
-
-        }
-    },
-    draw: function () {
-
-        var i = 0,
-            l = this.animObjects.length,
-            obj;
-
-        var left = this.startTime + this.duration - now(),
-            percent = ((left <= 0 ? 0 : left) / this.duration) || 0;
-
-        if (this.stopped) {
-            return false;
-        }
-
-        for (; i < l; i++) {
-            obj = this.animObjects[i];
-
-            obj.position = 1 - percent;
-
-            obj.now = ((obj.end - obj.start) * obj.position) + obj.start;
-
-            hAzzle.style(obj.elem, obj.prop, obj.now + 'px');
-
-            if (obj.position === 1) {
-                this.stop();
-            }
-        }
+    if (!this.rafId) {
+      this.rafId = hAzzle.requestFrame(raf);
     }
+  },
+
+  stop: function () {
+    if (hAzzle.isFunction(this.callback)) {
+      try {
+
+        this.callback();
+
+      } catch (ignored) {}
+    }
+
+    this.stopped = true;
+//
+//    if (window.cancelAnimationFrame) {
+      window.cancelAnimationFrame(this.rafId);
+  //  } else {
+    //  clearInterval(this.rafId);
+//    }
+
+    this.rafId = null;
+  },
+
+  setup: function () {
+    var i = 0,
+      props = Object.keys(this.options),
+      l = props.length,
+      prop,
+      elem = this.elem,
+      style = this.elem.style,
+      val;
+
+    this.startTime = now();
+
+    for (; i < l; i++) {
+      prop = props[i];
+      val = this.options[prop];
+
+      if (style[prop] !== undefined) {
+
+        var obj = {
+          elem: this.elem,
+          prop: prop,
+          options: this.options,
+          start: parseInt(hAzzle.css(elem, prop), 10),
+          end: parseInt(val, 10)
+        };
+
+        obj.now = obj.start;
+
+        this.animObjects.push(obj);
+      }
+
+    }
+  },
+  draw: function (tick) {
+    console.log(this.rafId);
+
+    var i = 0,
+      l = this.animObjects.length,
+      obj;
+
+    var left = this.startTime + this.duration - now(),
+      percent = ((left <= 0 ? 0 : left) / this.duration) || 0;
+
+    if (this.stopped) {
+      return false;
+    }
+
+    for (; i < l; i++) {
+      obj = this.animObjects[i];
+
+      obj.position = 1 - percent;
+
+      obj.now = ((obj.end - obj.start) * obj.position) + obj.start;
+
+      hAzzle.style(obj.elem, obj.prop, obj.now + 'px');
+
+      if (obj.position === 1) {
+        this.stop();
+      }
+    }
+  }
 };
 
+hAzzle.fx = fx;
 
 // animation function
 
 hAzzle.Core.animate = function (options, duration, callback) {
-    this.each(function (el) {
-        var fx = new hAzzle.fx(el, options, duration, callback);
-        fx.start();
-    });
+  this.each(function (el) {
+    var fx = new hAzzle.fx(el, options, duration, callback);
+    fx.start();
+  });
 };
