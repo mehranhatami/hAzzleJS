9c22b80a8e6df8382b23e815130d20ee3818ff58
KFlash <kflash123@gmail.com>
2014-07-22 15:05:29 +0800

    eventshook.js and aliases.js added
    Both modules are new and will not work for the events.js module that are
    on hAzzle just now. But both this modules are tested locally and are
    working!!!

--- modules/aliases.js
+++ modules/aliases.js
@@ -1,30 +1,18 @@
-// Aliases for events.js
+hAzzle.each( ("blur focus focusin focusout load resize scroll unload click dblclick " +
+	"mousedown mouseup mousemove mouseover mouseout mouseenter mouseleave " +
+	"change select submit keydown keypress keyup error contextmenu").split(" "),
+	function( name ) {
 
-hAzzle.each(('blur focus focusin focusout load resize scroll unload click dblclick ' +
-    'mousedown mouseup mousemove mouseover mouseout mouseenter mouseleave ' +
-    'change select submit keydown keypress keyup error contextmenu').split(' '), function (evt) {
-    hAzzle.Core[evt] = function (delegate, fn) {
-        return arguments.length > 0 ?
-            this.on(evt, delegate, fn) :
-            this.trigger(evt);
-    };
+	// Handle event binding
+	hAzzle.Core[ name ] = function( data, fn ) {
+		return arguments.length > 0 ?
+			this.on( name, null, data, fn ) :
+			this.trigger( name );
+	};
 });
 
 hAzzle.extend({
-
-    hover: function (fnOver, fnOut) {
-        return this.mouseenter(fnOver).mouseleave(fnOut || fnOver);
-    },
-
-    focus: function () {
-        return this.each(function (el) {
-            return el.focus();
-        });
-
-    },
-    blur: function () {
-        return this.each(function (el) {
-            return el.blur();
-        });
-    }
+	hover: function( fnOver, fnOut ) {
+		return this.mouseenter( fnOver ).mouseleave( fnOut || fnOver );
+	}
 });
--- modules/eventHooks.js
+++ modules/eventHooks.js
@@ -1,129 +1,238 @@
-/**
- * Collection of hAzzle eventHooks
+/** hAzzle eventHooks
  *
- * NOTE!! I will improve this hooks later on
- *
- * eventHooks are splitted into two groups
- *
- * - simulation
- * - special event types
- * - delegateType
- *
- * For event types:
- * ----------------
- *
- *  eventHook [ { OLD EVENT TYPE NAME } ] = {
- *
- *   name: { { NEW EVENT TYPE NAME } }
- *   handler: { { EVENT HANDLER }  }
- *  }
- *
- * Simulation:
- * ------------
- *
- *  eventHook [ { EVENT TYPE NAME } ] = {
- *
- *   simulate: { { YOUR FUNCTION HERE } }
- *
- *  }
- *
- * Delegated type:
- * ---------------
- *
- *  eventHook [ { OLD EVENT TYPE NAME } ] = {
- *   delegateType: { { NEW EVENT TYPE NAME } }
- *  }
- *
- *
- * 'delegateType' are used within event delegation. Say
- * you want mouseenter to work,
- * you then change mouseenter tobecome mouseover with
- * the eventHook
- *
+ * Note! In an attempt to be compatible with the
+ * jQuery API, our eventHooks are almost following
+ * the same pattern.
  */
-hAzzle.forOwn({
-    'pointerenter': 'pointerover',
-    'pointerleave': 'pointerout',
-    'mouseenter': 'mouseover',
-    'mouseleave': 'mouseout',
-}, function (orig, fix) {
-    hAzzle.eventHooks[orig] = {
-        specalEvents: {
-            name: fix,
-            handler: function (evt) {
-                var related = evt.relatedTarget,
-                    target = this;
-                return !related ? related === null : (related !== target && related.prefix !== 'xul' && !/document/.test(target.toString()) && !hAzzle.contains(related, target));
+hAzzle.extend({
+
+    'special': {
+
+        /**
+         * ScrollStart and ScrollStop special events
+         *
+         * Mehran!!  Here can you add in rAF you know :)
+         *
+         * 'latency' is the minimum time between the last scroll event and when the scrollstop event fires.
+         * should be replaced with performance.now() and rAF
+         *
+         */
+
+        'scrollstart': {
+
+            'setup': function (data) {
+
+                var _data = hAzzle.shallowCopy({
+
+                        latency: hAzzle.eventHooks.special.scrollstop.latency
+
+                    }, data),
+
+                    timer,
+                    self = this,
+                    args,
+                    handler = function (evt) {
+
+                        self = this,
+                            args = arguments;
+
+                        if (timer) {
+
+                            clearTimeout(timer);
+
+                        } else {
+
+                            evt.type = 'scrollstart';
+                            hAzzle.eventCore.dispatch.apply(self, args);
+                        }
+
+                        timer = setTimeout(function () {
+                            timer = null;
+                        }, _data.latency);
+                    };
+
+                hAzzle(this).on('scroll', null, handler).data('D' + hAzzle.now(), handler);
+            },
+            'shutdown': function () {
+                hAzzle(this).off('scroll', null, hAzzle(this).data('D' + hAzzle.now()));
             }
+        },
+		
+        // scrollstop
+
+        'scrollstop': {
+            latency: 250,
+            setup: function (data) {
+                var _data = hAzzle.shallowCopy({
+                    latency: hAzzle.eventHooks.special.scrollstop.latency
+                }, data);
+
+                var timer,
+                    handler = function (evt) {
+                        var _self = this,
+                            _args = arguments;
+
+                        if (timer) {
+                            clearTimeout(timer);
+                        }
+
+                        timer = setTimeout(function () {
+                            timer = null;
+                            evt.type = 'scrollstop';
+                            hAzzle.eventCore.dispatch.apply(_self, _args);
+                        }, _data.latency);
+                    };
+
+                hAzzle(this).on('scroll', null, handler).data('D' + hAzzle.now() + 1, handler);
+            },
+            'shutdown': function () {
+                hAzzle(this).off('scroll', null, hAzzle(this).data('D' + hAzzle.now() + 1));
+            }
+        },
+
+        'load': {
+            'noBubble': true
+        },
+        'focus': {
+            'trigger': function () {
+
+                if (this !== safeActiveElement() && this.focus) {
+                    this.focus();
+                    return false;
+                }
+            },
+            'delegateType': "focusin"
+        },
+        'blur': {
+            'trigger': function () {
+                if (this === safeActiveElement() && this.blur) {
+                    this.blur();
+                    return false;
+                }
+            },
+            'delegateType': "focusout"
+        },
+        'click': {
+
+            // For checkbox, fire native event so checked state will be right
+            trigger: function () {
+                if (this.type === "checkbox" && this.click && hAzzle.nodeName(this, "input")) {
+                    this.click();
+                    return false;
+                }
+            },
+
+            // For cross-browser consistency, don't fire native .click() on links
+            '_default': function (event) {
+                return hAzzle.nodeName(event.target, "a");
+            }
+        },
+
+        'beforeunload': {
+            'postDispatch': function (event) {
+                if (event.result !== undefined && event.originalEvent) {
+                    event.originalEvent.returnValue = event.result;
+                }
+            }
+        }
+    },
+
+    'simulate': function (type, elem, event, bubble) {
+
+        var e = hAzzle.shallowCopy(
+            new hAzzle.Event(),
+            event, {
+                type: type,
+                isSimulated: true,
+                originalEvent: {}
+            }
+        );
+        if (bubble) {
+            hAzzle.eventCore.trigger(e, null, elem);
+        } else {
+            hAzzle.eventCore.dispatch.call(elem, e);
+        }
+        if (e.isDefaultPrevented()) {
+            event.preventDefault();
+        }
+    }
+
+}, hAzzle.eventHooks);
+
+
+
+
+// Create mouseenter/leave events using mouseover/out and event-time checks
+// Support: Chrome 15+
+hAzzle.forOwn({
+    mouseenter: "mouseover",
+    mouseleave: "mouseout",
+    pointerenter: "pointerover",
+    pointerleave: "pointerout"
+}, function (fix, orig) {
+    hAzzle.eventHooks.special[orig] = {
+        delegateType: fix,
+        bindType: fix,
+
+        handle: function (evt) {
+            var ret,
+                target = this,
+                related = evt.relatedTarget,
+                handleObj = evt.handleObj;
+
+            if (!related || (related !== target && !hAzzle.contains(target, related))) {
+                evt.type = handleObj.origType;
+                ret = handleObj.handler.apply(this, arguments);
+                evt.type = fix;
+            }
+            return ret;
         }
     };
 });
 
 
+// Support: Firefox, Chrome, Safari
+// Create "bubbling" focus and blur events
 
-hAzzle.eventHooks.focus = {
-    delegateType: 'focusIn'
-};
-hAzzle.eventHooks.mouseenter = {
-    delegateType: 'mouseover'
-};
-hAzzle.eventHooks.blur = {
-    delegateType: 'focusout'
-};
+if (!hAzzle.eventCore.has['api-bubbles']) {
 
-var win = this,
-    push = Array.prototype.push,
-    needsFocusShim = hAzzle.isFirefox;
+    hAzzle.forOwn({
+        focus: "focusin",
+        blur: "focusout"
+    }, function (fix, orig) {
 
-if (needsFocusShim) {
+        var handler = function (event) {
+            hAzzle.eventHooks.simulate(fix, event.target, hAzzle.props.propFix(event), true);
+        };
 
-    var simulate = function (el, type) {
+        hAzzle.eventHooks.special[fix] = {
+            setup: function () {
+                var doc = this.ownerDocument || this,
+                    attaches = hAzzle.data(doc, fix);
 
-        var elfocus,
-            key,
-            focusEventType = (type === 'focusin') ? 'focus' : 'blur',
-            focusables = (function (el) {
-
-                var focusables = hAzzle(el).find('input'),
-                    selects = hAzzle(el).find('select');
-
-                if (selects.length) {
-                    push.apply(focusables, selects);
+                if (!attaches) {
+                    doc.addEventListener(orig, handler, true);
                 }
+                hAzzle.data(doc, fix, (attaches || 0) + 1);
+            },
+            shutdown: function () {
+                var doc = this.ownerDocument || this,
+                    attaches = hAzzle.data(doc, fix) - 1;
 
-                return focusables;
-            })(el),
-            cback = (function (type, focusEventType) {
-                return function () {
-                    if ((focusEventType === 'focus' && this === document.activeElement) || focusEventType === 'blur') {
-                        hAzzle(this).trigger(type);
-                    }
-                };
-            })(type, focusEventType),
+                if (!attaches) {
+                    doc.removeEventListener(orig, handler, true);
+                    hAzzle.dataRemove(doc, fix);
 
-            i = -1,
-
-            length = focusables.length;
-
-        key = '__' + focusEventType + 'Handled__';
-
-        while (++i < length) {
-
-            elfocus = focusables[i];
-
-            if (!elfocus.hasOwnProperty(key) || !elfocus[key]) {
-
-                elfocus[key] = true;
-
-                elfocus.addEventListener(focusEventType, cback, true);
-
+                } else {
+                    hAzzle.data(doc, fix, attaches);
+                }
             }
-        }
-    };
-
-    hAzzle.each(['focusin', 'focusout'], function (fix) {
-        hAzzle.eventHooks[fix] = {
-            simulate: simulate
         };
     });
+}
+
+function safeActiveElement() {
+    try {
+        return document.activeElement;
+    } catch (e) {}
 }
