2dcc9ed49f3a04af5839a66fca54efcdcb51b929
KFlash <kflash123@gmail.com>
2014-07-23 08:36:20 +0800

    hooks fix

--- modules/eventHooks.js
+++ modules/eventHooks.js
@@ -4,237 +4,228 @@
  * jQuery API, our eventHooks are almost following
  * the same pattern.
  */
-if (!hAzzle.eventCore) {
-  hAzzle.eventCore = {
-    has: function () {
-
-    }
-  };
-}
 hAzzle.extend({
 
-  'special': {
+    'special': {
 
-    /**
-     * ScrollStart and ScrollStop special events
-     *
-     * Mehran!!  Here can you add in rAF you know :)
-     *
-     * 'latency' is the minimum time between the last scroll event and when the scrollstop event fires.
-     * should be replaced with performance.now() and rAF
-     *
-     */
+        /**
+         * ScrollStart and ScrollStop special events
+         *
+         * Mehran!!  Here can you add in rAF you know :)
+         *
+         * 'latency' is the minimum time between the last scroll event and when the scrollstop event fires.
+         * should be replaced with performance.now() and rAF
+         *
+         */
 
-    'scrollstart': {
+        'scrollstart': {
 
-      'setup': function (data) {
+            'setup': function (data) {
 
-        var timer,
-          self = this,
-          args,
-          _data = hAzzle.shallowCopy({
+                var timer,
+                    self = this,
+                    args,
+                    _data = hAzzle.shallowCopy({
 
-            latency: hAzzle.eventHooks.special.scrollstop.latency
+                        latency: hAzzle.eventHooks.special.scrollstop.latency
 
-          }, data),
+                    }, data),
 
-          handler = function (evt) {
+                    handler = function (evt) {
 
-            self = this;
-            args = arguments;
+                        self = this;
+                        args = arguments;
 
-            if (timer) {
+                        if (timer) {
 
-              clearTimeout(timer);
+                            clearTimeout(timer);
 
-            } else {
+                        } else {
 
-              evt.type = 'scrollstart';
-              hAzzle.eventCore.dispatch.apply(self, args);
+                            evt.type = 'scrollstart';
+                            hAzzle.event.preparation.apply(self, args);
+                        }
+
+                        timer = setTimeout(function () {
+                            timer = null;
+                        }, _data.latency);
+                    };
+
+                hAzzle(this).on('scroll', null, handler).data('D' + hAzzle.now(), handler);
+            },
+            'shutdown': function () {
+                hAzzle(this).off('scroll', null, hAzzle(this).data('D' + hAzzle.now()));
             }
+        },
 
-            timer = setTimeout(function () {
-              timer = null;
-            }, _data.latency);
-          };
+        // scrollstop
 
-        hAzzle(this).on('scroll', null, handler).data('D' + hAzzle.now(), handler);
-      },
-      'shutdown': function () {
-        hAzzle(this).off('scroll', null, hAzzle(this).data('D' + hAzzle.now()));
-      }
-    },
+        'scrollstop': {
+            latency: 250,
+            setup: function (data) {
+                var _data = hAzzle.shallowCopy({
+                    latency: hAzzle.eventHooks.special.scrollstop.latency
+                }, data);
 
-    // scrollstop
+                var timer,
+                    handler = function (evt) {
+                        var _self = this,
+                            _args = arguments;
 
-    'scrollstop': {
-      latency: 250,
-      setup: function (data) {
-        var _data = hAzzle.shallowCopy({
-          latency: hAzzle.eventHooks.special.scrollstop.latency
-        }, data);
+                        if (timer) {
+                            clearTimeout(timer);
+                        }
 
-        var timer,
-          handler = function (evt) {
-            var _self = this,
-              _args = arguments;
+                        timer = setTimeout(function () {
+                            timer = null;
+                            evt.type = 'scrollstop';
+                            hAzzle.event.preparation.apply(_self, _args);
+                        }, _data.latency);
+                    };
 
-            if (timer) {
-              clearTimeout(timer);
+                hAzzle(this).on('scroll', null, handler).data('D' + hAzzle.now() + 1, handler);
+            },
+            'shutdown': function () {
+                hAzzle(this).off('scroll', null, hAzzle(this).data('D' + hAzzle.now() + 1));
             }
+        },
 
-            timer = setTimeout(function () {
-              timer = null;
-              evt.type = 'scrollstop';
-              hAzzle.eventCore.dispatch.apply(_self, _args);
-            }, _data.latency);
-          };
+        'load': {
+            'noBubble': true
+        },
+        'focus': {
+            'trigger': function () {
 
-        hAzzle(this).on('scroll', null, handler).data('D' + hAzzle.now() + 1, handler);
-      },
-      'shutdown': function () {
-        hAzzle(this).off('scroll', null, hAzzle(this).data('D' + hAzzle.now() + 1));
-      }
-    },
+                if (this !== safeActiveElement() && this.focus) {
+                    this.focus();
+                    return false;
+                }
+            },
+            'delegateType': 'focusin'
+        },
+        'blur': {
+            'trigger': function () {
+                if (this === safeActiveElement() && this.blur) {
+                    this.blur();
+                    return false;
+                }
+            },
+            'delegateType': 'focusout'
+        },
+        'click': {
 
-    'load': {
-      'noBubble': true
-    },
-    'focus': {
-      'trigger': function () {
+            // For checkbox, fire native event so checked state will be right
+            trigger: function () {
+                if (this.type === 'checkbox' && this.click && hAzzle.nodeName(this, 'input')) {
+                    this.click();
+                    return false;
+                }
+            },
 
-        if (this !== safeActiveElement() && this.focus) {
-          this.focus();
-          return false;
+            // For cross-browser consistency, don't fire native .click() on links
+            '_default': function (evt) {
+                return hAzzle.nodeName(evt.target, 'a');
+            }
+        },
+
+        'beforeunload': {
+            'postPrep': function (evt) {
+                if (evt.result !== undefined && evt.originalEvent) {
+                    evt.originalEvent.returnValue = evt.result;
+                }
+            }
         }
-      },
-      'delegateType': "focusin"
-    },
-    'blur': {
-      'trigger': function () {
-        if (this === safeActiveElement() && this.blur) {
-          this.blur();
-          return false;
-        }
-      },
-      'delegateType': "focusout"
-    },
-    'click': {
-
-      // For checkbox, fire native event so checked state will be right
-      trigger: function () {
-        if (this.type === "checkbox" && this.click && hAzzle.nodeName(this, "input")) {
-          this.click();
-          return false;
-        }
-      },
-
-      // For cross-browser consistency, don't fire native .click() on links
-      '_default': function (event) {
-        return hAzzle.nodeName(event.target, "a");
-      }
     },
 
-    'beforeunload': {
-      'postDispatch': function (event) {
-        if (event.result !== undefined && event.originalEvent) {
-          event.originalEvent.returnValue = event.result;
+    'simulate': function (type, elem, evt, bubble) {
+
+        var e = hAzzle.shallowCopy(
+            new hAzzle.Event(),
+            evt, {
+                type: type,
+                isSimulated: true,
+                originalEvent: {}
+            }
+        );
+        if (bubble) {
+            hAzzle.event.trigger(e, null, elem);
+        } else {
+            hAzzle.event.preparation.call(elem, e);
         }
-      }
+        if (e.isDefaultPrevented()) {
+            event.preventDefault();
+        }
     }
-  },
-
-  'simulate': function (type, elem, event, bubble) {
-
-    var e = hAzzle.shallowCopy(
-      new hAzzle.Event(),
-      event, {
-        type: type,
-        isSimulated: true,
-        originalEvent: {}
-      }
-    );
-    if (bubble) {
-      hAzzle.eventCore.trigger(e, null, elem);
-    } else {
-      hAzzle.eventCore.dispatch.call(elem, e);
-    }
-    if (e.isDefaultPrevented()) {
-      event.preventDefault();
-    }
-  }
 
 }, hAzzle.eventHooks);
 
 hAzzle.forOwn({
-  mouseenter: "mouseover",
-  mouseleave: "mouseout",
-  pointerenter: "pointerover",
-  pointerleave: "pointerout"
+    mouseenter: 'mouseover',
+    mouseleave: 'mouseout',
+    pointerenter: 'pointerover',
+    pointerleave: 'pointerout'
 }, function (fix, orig) {
-  hAzzle.eventHooks.special[orig] = {
-    delegateType: fix,
-    bindType: fix,
+    hAzzle.eventHooks.special[orig] = {
+        delegateType: fix,
+        bindType: fix,
 
-    handle: function (evt) {
-      var ret,
-        target = this,
-        related = evt.relatedTarget,
-        handleObj = evt.handleObj;
+        handle: function (evt) {
+            var ret,
+                target = this,
+                related = evt.relatedTarget,
+                handleObj = evt.handleObj;
 
-      if (!related || (related !== target && !hAzzle.contains(target, related))) {
-        evt.type = handleObj.origType;
-        ret = handleObj.handler.apply(this, arguments);
-        evt.type = fix;
-      }
-      return ret;
-    }
-  };
+            if (!related || (related !== target && !hAzzle.contains(target, related))) {
+                evt.type = handleObj.origType;
+                ret = handleObj.handler.apply(this, arguments);
+                evt.type = fix;
+            }
+            return ret;
+        }
+    };
 });
 
-
-// Support: Firefox, Chrome, Safari
-// Create "bubbling" focus and blur events
+/* =========================== INTERNAL ========================== */
 
 if (hAzzle.eventCore && !hAzzle.eventCore.has['api-bubbles']) {
 
-  hAzzle.forOwn({
-    focus: "focusin",
-    blur: "focusout"
-  }, function (fix, orig) {
+    hAzzle.forOwn({
+        focus: 'focusin',
+        blur: 'focusout'
+    }, function (fix, orig) {
 
-    var handler = function (event) {
-      hAzzle.eventHooks.simulate(fix, event.target, hAzzle.props.propFix(event), true);
-    };
+        var handler = function (evt) {
+            hAzzle.eventHooks.simulate(fix, evt.target, hAzzle.props.propFix(evt), true);
+        };
 
-    hAzzle.eventHooks.special[fix] = {
-      setup: function () {
-        var doc = this.ownerDocument || this,
-          attaches = hAzzle.data(doc, fix);
+        hAzzle.eventHooks.special[fix] = {
+            setup: function () {
+                var doc = this.ownerDocument || this,
+                    attaches = hAzzle.data(doc, fix);
 
-        if (!attaches) {
-          doc.addEventListener(orig, handler, true);
-        }
-        hAzzle.data(doc, fix, (attaches || 0) + 1);
-      },
-      shutdown: function () {
-        var doc = this.ownerDocument || this,
-          attaches = hAzzle.data(doc, fix) - 1;
+                if (!attaches) {
+                    doc.addEventListener(orig, handler, true);
+                }
+                hAzzle.data(doc, fix, (attaches || 0) + 1);
+            },
+            shutdown: function () {
+                var doc = this.ownerDocument || this,
+                    attaches = hAzzle.data(doc, fix) - 1;
 
-        if (!attaches) {
-          doc.removeEventListener(orig, handler, true);
-          hAzzle.dataRemove(doc, fix);
+                if (!attaches) {
+                    doc.removeEventListener(orig, handler, true);
+                    hAzzle.dataRemove(doc, fix);
 
-        } else {
-          hAzzle.data(doc, fix, attaches);
-        }
-      }
-    };
-  });
+                } else {
+                    hAzzle.data(doc, fix, attaches);
+                }
+            }
+        };
+    });
 }
 
 function safeActiveElement() {
-  try {
-    return document.activeElement;
-  } catch (e) {}
+    try {
+        return document.activeElement;
+    } catch (e) {}
 }
