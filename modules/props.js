674552cdb95cf92ff8c11896797085bbd77e5c9f
KFlash <kflash123@gmail.com>
2014-07-23 11:59:13 +0800

    props.js fix

--- modules/props.js
+++ modules/props.js
@@ -1,95 +1,93 @@
 /**
  * Events properties
  *
- * Note! This is extendable through plugins
+ * Note! This is NOT extendable through plugins
  *
  */
 var keyRegex = /key/i,
     mouseRegex = /click|mouse(?!(.*wheel|scroll))|menu|drag|drop/i,
+    mousewheelRegex = /mouse.*(wheel|scroll)/i,
     textRegex = /^text/i,
     touchRegex = /^touch|^gesture/i,
     popstateRegex = /^popstate$/i,
     msgRegex = /^message$/i,
 
     // a whitelist of properties for different event types
-    commonProps = ('altKey attrChange attrName bubbles cancelable ctrlKey currentTarget ' +
+    commonProps = ('altKey attrChange cancelable attrName bubbles cancelable ctrlKey currentTarget ' +
         'detail eventPhase getModifierState isTrusted metaKey relatedNode relatedTarget shiftKey ' +
         'srcElement target timeStamp type view which propertyName').split(' '),
-    keyProps = 'char charCode key keyCode'.split(' '),
+    keyProps = 'char charCode key keyCode keyIdentifier keyLocation location'.split(' '),
     mouseProps = 'button buttons clientX clientY offsetX offsetY pageX pageY ' +
-    'screenX screenY toElement'.split(' '),
+    'screenX screenY toElement dataTransfer fromElement'.split(' '),
+    mouseWheelProps = mouseProps.concat('wheelDelta wheelDeltaX wheelDeltaY wheelDeltaZ deltaY deltaX deltaZ' +
+        'axis'.split(' ')),
     touchProps = 'touches targetTouches changedTouches scale rotation'.split(' '),
     messageProps = 'data origin source'.split(' '),
     textProps = 'data',
     stateProps = 'state';
 
+// Mozilla and webKit have special events
+
+// Firefox
+
+if (hAzzle.isFirefox) {
+    mouseProps = mouseProps.concat(' mozMovementY mozMovementX'.split(' '))
+}
+// webKit 
+if (hAzzle.isChrome || hAzzle.isOpera) {
+    mouseProps = mouseProps.concat(' webkitMovementY webkitMovementX'.split(' '))
+}
+
 hAzzle.props = {
 
     hookers: [{ // key events
-        reg: keyRegex,
-        props: keyProps,
-        filter: function (evt, original) {
+            reg: keyRegex,
+            props: keyProps,
+            filter: function (evt, original) {
 
-            // Add which for key events
-            if (evt.which === null) {
-                evt.which = original.charCode !== null ? original.charCode : original.keyCode;
+                // Add which for key events
+                if (evt.which === null) {
+                    evt.which = original.charCode !== null ? original.charCode : original.keyCode;
+                }
+
+                return evt;
             }
+        }, { // mouse events
+            reg: mouseRegex,
+            props: mouseProps,
+            filter: mousescroll
+        },
 
-            return evt;
-        }
-    }, { // mouse events
-        reg: mouseRegex,
-        props: mouseProps,
-        filter: function (evt, original) {
-
-            var evtDoc, doc, body,
-                button = original.button;
-
-            // Calculate pageX/Y if missing and clientX/Y available
-            if (evt.pageX === null && original.clientX !== null) {
-                evtDoc = evt.target.ownerDocument || document;
-                doc = evtDoc.documentElement;
-                body = evtDoc.body;
-
-                evt.pageX = original.clientX +
-                    (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
-                    (doc && doc.clientLeft || body && body.clientLeft || 0);
-                evt.pageY = original.clientY +
-                    (doc && doc.scrollTop || body && body.scrollTop || 0) -
-                    (doc && doc.clientTop || body && body.clientTop || 0);
+        { // Mouse wheel / scroll events
+            reg: mousewheelRegex,
+            props: mouseWheelProps,
+            filter: mousescroll
+        }, { // TextEvent
+            reg: textRegex,
+            props: textProps,
+            filter: function (evt) {
+                return evt;
             }
-
-            if (!evt.which && button !== undefined) {
-                evt.which = (button & 1 ? 1 : (button & 2 ? 3 : (button & 4 ? 2 : 0)));
+        }, { // Touch and gesture events
+            reg: touchRegex,
+            props: touchProps,
+            filter: function (evt) {
+                return evt;
             }
-
-            return evt;
+        }, { // Message events
+            reg: msgRegex,
+            props: messageProps,
+            filter: function (evt) {
+                return evt;
+            }
+        }, { // Popstate events
+            reg: popstateRegex,
+            props: stateProps,
+            filter: function (evt) {
+                return evt;
+            }
         }
-    }, { // TextEvent
-        reg: textRegex,
-        props: textProps,
-        filter: function (evt) {
-            return evt;
-        }
-    }, { // touch and gesture events
-        reg: touchRegex,
-        props: touchProps,
-        filter: function (evt) {
-            return evt;
-        }
-    }, { // message events
-        reg: msgRegex,
-        props: messageProps,
-        filter: function (evt) {
-            return evt;
-        }
-    }, { // popstate events
-        reg: popstateRegex,
-        props: stateProps,
-        filter: function (evt) {
-            return evt;
-        }
-    }],
+    ],
 
     fixedEvents: {},
 
@@ -144,3 +142,34 @@ hAzzle.props = {
         return fE.filter ? fE.filter(evt, originalEvent) : evt;
     }
 };
+
+/* =========================== INTERNAL ========================== */
+
+// Same for mouse, mouseWheel and mouseScroll
+
+var mousescroll = function (evt, original) {
+    alert(evt)
+    var evtDoc, doc, body,
+        button = original.button;
+
+
+    // Calculate pageX/Y if missing and clientX/Y available
+    if (evt.pageX === null && original.clientX !== null) {
+        evtDoc = evt.target.ownerDocument || document;
+        doc = evtDoc.documentElement;
+        body = evtDoc.body;
+
+        evt.pageX = original.clientX +
+            (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+            (doc && doc.clientLeft || body && body.clientLeft || 0);
+        evt.pageY = original.clientY +
+            (doc && doc.scrollTop || body && body.scrollTop || 0) -
+            (doc && doc.clientTop || body && body.clientTop || 0);
+    }
+
+    if (!evt.which && button !== undefined) {
+        evt.which = (button & 1 ? 1 : (button & 2 ? 3 : (button & 4 ? 2 : 0)));
+    }
+
+    return evt;
+}
