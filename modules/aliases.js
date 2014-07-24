33e18cace37903fe0b2a96c7dd6922647d255068
KFlash <kflash123@gmail.com>
2014-07-24 10:49:50 +0800

    reduced complexity

--- modules/aliases.js
+++ modules/aliases.js
@@ -12,7 +12,139 @@ hAzzle.each( ("blur focus focusin focusout load resize scroll unload click dblcl
 });
 
 hAzzle.extend({
+
+    on: function (types, selector, data, fn, /*INTERNAL*/ one) {
+
+        var origFn, type;
+
+        if (typeof types === 'object') {
+
+            if (typeof selector !== 'string') {
+
+                data = data || selector;
+                selector = undefined;
+            }
+
+            for (type in types) {
+
+                this.on(type, selector, data, types[type], one);
+            }
+            return this;
+        }
+
+        // Has to be boolean values, else
+        // everything break. So keep '==' and not '==='
+
+        if (data == null && fn == null) {
+
+            fn = selector;
+            data = selector = undefined;
+
+        } else if (fn == null) {
+
+            if (typeof selector === 'string') {
+
+                fn = data;
+                data = undefined;
+
+            } else {
+
+                fn = data;
+                data = selector;
+                selector = undefined;
+            }
+        }
+
+        if (fn === false) {
+
+            fn = returnFalse;
+
+        } else if (!fn) {
+
+            return this;
+        }
+
+        // One
+
+        if (one === 1) {
+
+            origFn = fn;
+
+            fn = once(fn);
+
+            // Use same guid so caller can remove using origFn
+            fn.guid = origFn.guid || (origFn.guid = hAzzle.eventCore.setID());
+        }
+
+        return this.each(function () {
+            hAzzle.event.add(this, types, fn, data, selector);
+        });
+    },
+    one: function (types, selector, data, fn) {
+        return this.on(types, selector, data, fn, 1);
+    },
+    off: function (types, selector, fn) {
+        var handleObj, type;
+        if (types && types.preventDefault && types.handleObj) {
+            handleObj = types.handleObj;
+            hAzzle(types.delegateTarget).off(
+                handleObj.namespace ?
+                handleObj.origType + '.' + handleObj.namespace :
+                handleObj.origType,
+                handleObj.selector,
+                handleObj.handler
+            );
+            return this;
+        }
+        if (typeof types === 'object') {
+            // ( types-object [, selector] )
+            for (type in types) {
+                this.off(type, selector, types[type]);
+            }
+            return this;
+        }
+        if (selector === false || typeof selector === 'function') {
+            // ( types [, fn] )
+            fn = selector;
+            selector = undefined;
+        }
+        if (fn === false) {
+            fn = returnFalse;
+        }
+        return this.each(function () {
+            hAzzle.event.remove(this, types, fn, selector);
+        });
+    },
+
+    trigger: function (type, data) {
+        return this.each(function () {
+            hAzzle.event.trigger(type, data, this);
+        });
+    },
+    triggerHandler: function (type, data) {
+        var elem = this[0];
+        if (elem) {
+            return hAzzle.event.trigger(type, data, elem, true);
+        }
+    },
+
 	hover: function( fnOver, fnOut ) {
 		return this.mouseenter( fnOver ).mouseleave( fnOut || fnOver );
 	}
+
 });
+
+/* ============================ UTILITY METHODS =========================== */
+
+function returnFalse() {
+    return false;
+}
+
+function once(fn) {
+
+    return function (evt) {
+        // wrap the handler in a handler that does a remove as well
+        hAzzle().off(evt);
+        return fn.apply(this, arguments);
+    };
+}
--- modules/events.js
+++ modules/events.js
@@ -56,7 +56,7 @@ var doc = this.document,
      * - feature / bug checks
      */
 
-    eventCore = {
+    eventCore = hAzzle.eventCore = {
 
         version: 'hAzzleEvents-0.45a',
 
@@ -645,131 +645,8 @@ hAzzle.Event.prototype = {
 
             this.cancelBubble = true;
         }
-
     }
 };
-
-/* ============================ CORE FUNCTIONS =========================== */
-
-hAzzle.extend({
-
-    on: function (types, selector, data, fn, /*INTERNAL*/ one) {
-
-        var origFn, type;
-
-
-
-        if (typeof types === 'object') {
-
-            if (typeof selector !== 'string') {
-
-                data = data || selector;
-                selector = undefined;
-            }
-
-            for (type in types) {
-
-                this.on(type, selector, data, types[type], one);
-            }
-            return this;
-        }
-
-        // Has to be boolean values, else
-        // everything break. So keep '==' and not '==='
-
-        if (data == null && fn == null) {
-
-            fn = selector;
-            data = selector = undefined;
-
-        } else if (fn == null) {
-
-            if (typeof selector === 'string') {
-
-                fn = data;
-                data = undefined;
-
-            } else {
-
-                fn = data;
-                data = selector;
-                selector = undefined;
-            }
-        }
-
-        if (fn === false) {
-
-            fn = returnFalse;
-
-        } else if (!fn) {
-
-            return this;
-        }
-
-        // One
-
-        if (one === 1) {
-
-            origFn = fn;
-
-            fn = once(fn);
-
-            // Use same guid so caller can remove using origFn
-            fn.guid = origFn.guid || (origFn.guid = eventCore.setID());
-        }
-
-        return this.each(function () {
-            hAzzle.event.add(this, types, fn, data, selector);
-        });
-    },
-    one: function (types, selector, data, fn) {
-        return this.on(types, selector, data, fn, 1);
-    },
-    off: function (types, selector, fn) {
-        var handleObj, type;
-        if (types && types.preventDefault && types.handleObj) {
-            handleObj = types.handleObj;
-            hAzzle(types.delegateTarget).off(
-                handleObj.namespace ?
-                handleObj.origType + '.' + handleObj.namespace :
-                handleObj.origType,
-                handleObj.selector,
-                handleObj.handler
-            );
-            return this;
-        }
-        if (typeof types === 'object') {
-            // ( types-object [, selector] )
-            for (type in types) {
-                this.off(type, selector, types[type]);
-            }
-            return this;
-        }
-        if (selector === false || typeof selector === 'function') {
-            // ( types [, fn] )
-            fn = selector;
-            selector = undefined;
-        }
-        if (fn === false) {
-            fn = returnFalse;
-        }
-        return this.each(function () {
-            hAzzle.event.remove(this, types, fn, selector);
-        });
-    },
-
-    trigger: function (type, data) {
-        return this.each(function () {
-            hAzzle.event.trigger(type, data, this);
-        });
-    },
-    triggerHandler: function (type, data) {
-        var elem = this[0];
-        if (elem) {
-            return hAzzle.event.trigger(type, data, elem, true);
-        }
-    }
-});
 
 /* ============================ UTILITY METHODS =========================== */
 
@@ -779,15 +656,6 @@ function returnTrue() {
 
 function returnFalse() {
     return false;
-}
-
-function once(fn) {
-
-    return function (evt) {
-        // wrap the handler in a handler that does a remove as well
-        hAzzle().off(evt);
-        return fn.apply(this, arguments);
-    };
 }
 
 // Get document from element
@@ -802,8 +670,6 @@ function newNS(ns) {
     return new RegExp('(^|\\.)' + ns.join('\\.(?:.*\\.|)') + '(\\.|$)');
 }
 
-
-
 /**
  * Get correct 'event' for the trigger() function
  *
@@ -813,7 +679,6 @@ function newNS(ns) {
  * @param {Object|Undefined} ns
  * @param {String} type
  * @return {Object}  
-
  */
 
 function getEvent(elem, evt, handler, ns, type) {
@@ -829,7 +694,6 @@ function getEvent(elem, evt, handler, ns, type) {
         // try to use evt.srcElement if we can
 
         evt.target = evt.srcElement || elem;
-
     }
 
     return evt;
