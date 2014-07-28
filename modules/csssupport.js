283cf3647f7b1a73419042f5f952b146b4b2f642
KFlash <kflash123@gmail.com>
2014-07-28 16:28:17 +0800

    csssupport.js cleanup

--- modules/csssupport.js
+++ modules/csssupport.js
@@ -1,9 +1,9 @@
 var cssSupport = {
 
-      // Check for getComputedStyle support
+    // Check for getComputedStyle support
 
-     'api-gCS': !!document.defaultView.getComputedStyle
-}
+    'api-gCS': !!document.defaultView.getComputedStyle
+};
 
 /* ============================ FEATURE / BUG DETECTION =========================== */
 
@@ -11,33 +11,31 @@ hAzzle.assert(function(div) {
 
     var style = div.style;
 
-    // BackgroundPositionXY 
-
-    hAzzle.applyCSSSupport('borderImage', div.style.backgroundPositionX !== null);
-
     // BorderImage support
-   cssSupport['borderImage'] = style.borderImage !== undefined ||
+
+    cssSupport.borderImage = style.borderImage !== undefined ||
         style.MozBorderImage !== undefined ||
         style.WebkitBorderImage !== undefined ||
         style.msBorderImage !== undefined;
-   
+
     // BoxShadow
-    cssSupport['boxShadow'] = style.BoxShadow !== undefined ||
+
+    cssSupport.boxShadow = style.BoxShadow !== undefined ||
         style.MsBoxShadow !== undefined ||
         style.WebkitBoxShadow !== undefined ||
         style.OBoxShadow !== undefined;
 
-// Transition
+    // Transition
 
-	cssSupport['transition'] = style.transition !== undefined || 
-                            style.WebkitTransition !== undefined || 
-							style.MozTransition !== undefined || 
-							style.MsTransition !== undefined || 
-							style.OTransition !== undefined;
+    cssSupport.transition = style.transition !== undefined ||
+        style.WebkitTransition !== undefined ||
+        style.MozTransition !== undefined ||
+        style.MsTransition !== undefined ||
+        style.OTransition !== undefined;
 
     // textShadow support
 
-     cssSupport['textShadow'] = style.textShadow === '';
+    cssSupport.textShadow = style.textShadow === '';
 });
 
 
@@ -67,63 +65,20 @@ hAzzle.transition = cssSupport.transition;
 
 hAzzle.assert(function(div) {
 
-    // Support: IE9-11+
+    var pixelPositionVal, boxSizingReliableVal;
 
-    cssSupport['bug-clearCloneStyle'] = div.style.backgroundClip === 'content-box';
-
-    var pixelPositionVal, boxSizingReliableVal,
-        container = document.createElement('div');
-
-    container.style.cssText = 'border:0;width:0;height:0;top:0;left:-9999px;margin-top:1px;' +
-        'position:absolute';
-
-    container.appendChild(div);
-
-    function computePixelPositionAndBoxSizingReliable() {
-        div.style.cssText =
-            // Support: Firefox<29, Android 2.3
-            // Vendor-prefix box-sizing
-            '-webkit-box-sizing:border-box;-moz-box-sizing:border-box;' +
-            'box-sizing:border-box;display:block;margin-top:1%;top:1%;' +
-            'border:1px;padding:1px;width:4px;position:absolute';
-        div.innerHTML = '';
-        docElem.appendChild(container);
-
-        var divStyle = window.getComputedStyle(div, null);
-        pixelPositionVal = divStyle.top !== '1%';
-        boxSizingReliableVal = divStyle.width === '4px';
-
-        docElem.removeChild(container);
-    }
-
-    // Check if we support getComputedStyle
+    div.style.backgroundClip = "content-box";
+    div.cloneNode(true).style.backgroundClip = "";
+    cssSupport['bug-clearCloneStyle'] = div.style.backgroundClip === "content-box";
 
     if (cssSupport['api-gCS']) {
 
-        cssSupport['api-pixelPosition'] = (function() {
-            computePixelPositionAndBoxSizingReliable();
-            return pixelPositionVal;
-        })();
-
-        cssSupport['api-boxSizingReliable'] = (function() {
-            if (boxSizingReliableVal === null) {
-                computePixelPositionAndBoxSizingReliable();
-            }
-            return boxSizingReliableVal;
-        })();
-
-        cssSupport['api-reliableMarginRight'] = (function() {
-            var ret, marginDiv = div.appendChild(document.createElement('div'));
-            marginDiv.style.cssText = div.style.cssText =
-                '-webkit-box-sizing:content-box;-moz-box-sizing:content-box;' +
-                'box-sizing:content-box;display:block;margin:0;border:0;padding:0';
-            marginDiv.style.marginRight = marginDiv.style.width = '0';
-            div.style.width = '1px';
-            docElem.appendChild(container);
-            ret = !parseFloat(window.getComputedStyle(marginDiv, null).marginRight);
-            docElem.removeChild(container);
-            return ret;
-        })();
+        div.style.cssText = 'border:1px;padding:1px;width:4px;position:absolute';
+        var divStyle = window.getComputedStyle(div, null);
+        pixelPositionVal = divStyle.top !== '1%';
+        boxSizingReliableVal = divStyle.width === '4px';
+        cssSupport['api-pixelPosition'] = pixelPositionVal;
+        cssSupport['api-boxSizingReliable'] = boxSizingReliableVal;
     }
 });
 
@@ -132,4 +87,3 @@ hAzzle.assert(function(div) {
 hAzzle.clearCloneStyle = cssSupport['bug-clearCloneStyle'];
 hAzzle.pixelPosition = cssSupport['api-pixelPosition'];
 hAzzle.boxSizingReliable = cssSupport['api-boxSizingReliable'];
-hAzzle.reliableMarginRight = cssSupport['api-reliableMarginRight'];
