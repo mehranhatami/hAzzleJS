3c1adb517696d88e774fc1923840df4d45b39ba7
KFlash <kflash123@gmail.com>
2014-08-01 07:56:32 +0800

    quick fix

--- modules/booleans.js
+++ modules/booleans.js
@@ -1,29 +1,20 @@
 // boolean.js
 // Shared with attributes.js and compile.js
-(function() {
 
-    var boolAttr = hAzzle.boolAttr = {}, // Boolean attributes
-        boolElem = hAzzle.boolElem = {}, // Boolean elements
+    hAzzle.boolAttr = {}, // Boolean attributes
+    hAzzle.boolElem = {}, // Boolean elements
 
-        // Booleans
+    // Boolean attributes and elements
 
-        bools = [
+    hAzzle.each([
             'multiple', 'selected', 'checked', 'disabled', 'readOnly', 'required',
             'async', 'autofocus', 'compact', 'nowrap', 'declare', 'noshade',
             'noresize', 'defaultChecked', 'autoplay', 'controls', 'defer',
             'hidden', 'ismap', 'loop', 'scoped', 'open'
-        ],
-        belem = ['input', 'select', 'option', 'textarea', 'button', 'form', 'details'];
-
-
-    // Boolean attributes and elements
-
-    hAzzle.each(bools, function() {
-        boolAttr[this] = this;
+        ], function() {
+        hAzzle.boolAttr[this] = this;
     });
 
-    hAzzle.each(belem, function() {
-        boolElem[this.toUpperCase()] = true;
+    hAzzle.each(['input', 'select', 'option', 'textarea', 'button', 'form', 'details'], function() {
+        hAzzle.boolElem[this.toUpperCase()] = true;
     });
-
-}());
