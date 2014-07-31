158790bf0daff5108d76c248f832270171ee6a8c
KFlash <kflash123@gmail.com>
2014-08-01 07:53:50 +0800

    typo fix

--- modules/booleans.js
+++ modules/booleans.js
@@ -1,24 +1,29 @@
 // boolean.js
 // Shared with attributes.js and compile.js
+(function() {
 
-var boolAttr = hAzzle.boolAttr = {}, // Boolean attributes
-    boolElem = hAzzle.boolElem = {}, // Boolean elements
-	
-	// Booleans
+    var boolAttr = hAzzle.boolAttr = {}, // Boolean attributes
+        boolElem = hAzzle.boolElem = {}, // Boolean elements
 
-    bools = [
-        'multiple', 'selected', 'checked', 'disabled', 'readOnly', 'required',
-        'async', 'autofocus', 'compact', 'nowrap', 'declare', 'noshade',
-        'noresize', 'defaultChecked', 'autoplay', 'controls', 'defer',
-        'hidden', 'ismap', 'loop', 'scoped', 'open'
-    ];
-	
-// Boolean attributes and elements
+        // Booleans
 
-hAzzle.each(bools, function() {
-    boolAttr[this] = this;
-});
+        bools = [
+            'multiple', 'selected', 'checked', 'disabled', 'readOnly', 'required',
+            'async', 'autofocus', 'compact', 'nowrap', 'declare', 'noshade',
+            'noresize', 'defaultChecked', 'autoplay', 'controls', 'defer',
+            'hidden', 'ismap', 'loop', 'scoped', 'open'
+        ],
+        belem = ['input', 'select', 'option', 'textarea', 'button', 'form', 'details'];
 
-hAzzle.each(['input', 'select', 'option', 'textarea', 'button', 'form', 'details'], function() {
-    boolElem[this.toUpperCase()] = true;
-});	
+
+    // Boolean attributes and elements
+
+    hAzzle.each(bools, function() {
+        boolAttr[this] = this;
+    });
+
+    hAzzle.each(belem, function() {
+        boolElem[this.toUpperCase()] = true;
+    });
+
+}());
