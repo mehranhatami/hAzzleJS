4fd41dd97b8d59b205c4d6ab8b5f3e6292a3b573
KFlash <kflash123@gmail.com>
2014-08-14 07:11:45 +0800

    effects.js
    I still working on the animation engine. Things take time, but for now I
    uploaded easing.js and effects.js

---
+++ modules/effects.js
@@ -0,0 +1,74 @@
+// effects.js
+hAzzle.each({
+
+    /**
+     * Display or hide the matched elements with a sliding motion
+     */
+
+    slideToggle: createAnimation('toggle'),
+
+    /**
+     * Display the matched elements by fading them to opaque.
+     */
+
+    fadeIn: {
+        opacity: 'show'
+    },
+
+    /**
+     * Hide the matched elements by fading them to transparent.
+     */
+
+    fadeOut: {
+        opacity: 'hide'
+    },
+
+    /**
+     * Display or hide the matched elements by animating their opacity.
+     */
+
+    fadeToggle: {
+        opacity: 'toggle'
+    },
+
+    /**
+     * Display the matched elements with a sliding motion.
+     */
+
+    slideDown: createAnimation('show'),
+
+    /**
+     * Hide the matched elements with a sliding motion
+     */
+
+    slideUp: createAnimation('hide'),
+
+
+
+}, function(props, name) {
+
+    hAzzle.Core[name] = function(speed, easing, callback) {
+        return this.animate(props, speed, easing, callback);
+    };
+});
+
+function createAnimation(type, includeWidth) {
+
+    var which, i = 0,
+        attrs = {
+            height: type
+        };
+
+    includeWidth = includeWidth ? 1 : 0;
+
+    for (; i < 4; i += 2 - includeWidth) {
+        which = directions[i];
+        attrs['margin' + which] = attrs['padding' + which] = type;
+    }
+
+    if (includeWidth) {
+        attrs.opacity = attrs.width = type;
+    }
+
+    return attrs;
+}
