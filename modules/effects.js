e8868c31184b96c96a085405889921236ba75f43
KFlash <kflash123@gmail.com>
2014-08-28 07:24:16 +0800

    quick fix

--- modules/effects.js
+++ modules/effects.js
@@ -1,78 +1,44 @@
-// effects.js
-var expat = [ "Top", "Right", "Bottom", "Left" ];
-hAzzle.each({
-
-    /**
-     * Display or hide the matched elements with a sliding motion
-     */
-
-    slideToggle: createAnimation('toggle'),
-
-    /**
-     * Display the matched elements by fading them to opaque.
-     */
-
-    fadeIn: {
-        opacity: 'show'
-    },
-
-    /**
-     * Hide the matched elements by fading them to transparent.
-     */
-
-    fadeOut: {
-        opacity: 'hide'
-    },
-
-    /**
-     * Display or hide the matched elements by animating their opacity.
-     */
-
-    fadeToggle: {
-        opacity: 'toggle'
-    },
-
-    /**
-     * Display the matched elements with a sliding motion.
-     */
-
-    slideDown: createAnimation('show'),
-
-    /**
-     * Hide the matched elements with a sliding motion
-     */
-
-    slideUp: createAnimation('hide'),
 
 
 
-}, function(props, name) {
 
-    hAzzle.Core[name] = function(speed, easing, callback) {
-        return this.animate(props, { duration: speed,
-			                         easing: easing,
-			                         complete: callback
-			                         });
-    };
-});
 
-function createAnimation(type, includeWidth) {
 
-    var which, i = 0,
-        attrs = {
-            height: type
-        };
+var cssExpand = [ "Top", "Right", "Bottom", "Left" ];
 
-    includeWidth = includeWidth ? 1 : 0;
 
-    for (; i < 4; i += 2 - includeWidth) {
-        which = expat[i];
-        attrs['margin' + which] = attrs['padding' + which] = type;
-    }
 
-    if (includeWidth) {
-        attrs.opacity = attrs.width = type;
-    }
+function genFx( type, includeWidth ) {
+	var which,
+		i = 0,
+		attrs = { height: type };
 
-    return attrs;
+	// If we include width, step value is 1 to do all cssExpand values,
+	// otherwise step value is 2 to skip over Left and Right
+	includeWidth = includeWidth ? 1 : 0;
+	for ( ; i < 4 ; i += 2 - includeWidth ) {
+		which = cssExpand[ i ];
+		attrs[ "margin" + which ] = attrs[ "padding" + which ] = type;
+	}
+
+	if ( includeWidth ) {
+		attrs.opacity = attrs.width = type;
+	}
+
+	return attrs;
 }
+
+
+// Generate shortcuts for custom animations
+hAzzle.each({
+	slideDown: genFx( "show"),
+	slideUp: genFx( "hide"),
+	slideToggle: genFx( "toggle"),
+	fadeIn: { opacity: "show" },
+	fadeOut: { opacity: "hide" },
+	fadeToggle: { opacity: "toggle" }
+}, function( props, name ) {
+	hAzzle.Core[ name ] = function( speed, easing, callback ) {
+		return this.animate( props, speed, easing, callback );
+	};
+});
