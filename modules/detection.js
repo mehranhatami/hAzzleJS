e2d352629c69b33b88ef3a4d48487257cc76c8ac
KFlash <kflash123@gmail.com>
2014-08-28 17:27:38 +0800

    detection.js update

--- modules/detection.js
+++ modules/detection.js
@@ -1,21 +1,48 @@
+var i, ua = navigator.userAgent,
+    Detection = {
 
-var i, ua = navigator.userAgent, 
-Detection = {
+        Mobile: /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(ua),
+        Android: /Android/i.test(ua),
 
-    mobile: /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(ua),
-    android: /Android/i.test(ua),
+        // Feature detection
 
-    // Feature detection
-
-    opera: !!window.opera || ua.indexOf(' OPR/') >= 0,
-    Firefox: typeof InstallTrigger !== 'undefined', // Firefox
-    Chrome: !!window.chrome && !window.opera || ua.indexOf(' OPR/') >= 0, // Chrome 1+
-    Safari: Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0,
-    IE: false || !!document.documentMode // IE
-}
+        opera: !!window.opera || ua.indexOf(' OPR/') >= 0,
+        Firefox: typeof InstallTrigger !== 'undefined', // Firefox
+        Chrome: !!window.chrome && !window.opera || ua.indexOf(' OPR/') >= 0, // Chrome 1+
+        Safari: Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0,
+        IE: false || !!document.documentMode // IE
+    }
 
 // Expose
 
 for (i in Detection) {
     hAzzle['is' + i] = Detection[i]
+}
+
+// Get correct mobile / Tablet device
+
+hAzzle.getMobile = function() {
+
+    var agen = ua.toLowerCase();
+
+    // Return if no mobile device
+
+    if (!hAzzle.isMobile) {
+        return null;
+    }
+
+    if (agent.indexOf('iphone') !== -1 || agent.indexOf('ipad') !== -1) {
+        return 'ios';
+    }
+
+    if (agent.indexOf('android') !== -1 || agent.indexOf('applewebkit') !== -1) {
+        return 'android';
+
+    }
+
+    if (agent.indexOf('msie') !== -1) {
+        return 'winMobile';
+    }
+
+    return null;
 }
