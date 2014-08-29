ff53415a0eeeedd3e866dc87ba118a60420920fd
KFlash <kflash123@gmail.com>
2014-08-30 06:22:27 +0800

    quick percentage.js fix

--- modules/percentage.js
+++ modules/percentage.js
@@ -13,65 +13,53 @@ Percentage.prototype = {
 
     init: function(elem, to, options) {
 
-        length = dictionary.length
+        length = dictionary.length;
 
         this.elem = elem;
         this.complete = options.callback;
         this.completeParams = options.callbackParams;
         this.originalState = [];
-		
-	        var i = 0,
-            ar = [],
+
+        var i = 0, ar = [],
             prop, begin, end,
             newbs = to.to,
             from = to.from,
-			style = elem.style,
+            style = elem.style,
             self = hAzzle.private(elem, 'fxDta', dictionary[length++] = this),
             easing = options.ease || hAzzle.defaultEasing,
-            duration = options.duration || defaultDuration;
 
-        // Support for jQuery's named durations.
+            // Support for jQuery's named durations.
+            // Duration '0' will likely never happen, it will see it as false,
+            // and set 'defaultDuration'
 
-        switch (duration.toString().toLowerCase()) {
-            case 'fast':
-                duration = 200;
-                break;
-
-            case 'normal':
-                duration = defaultDuration;
-                break;
-
-            case 'slow':
-                duration = 600;
-                break;
-
-            default:
-
-                // Default to 1 if the user is attempting to set a duration of 0 (in order to 
-                // produce an immediate style change).
-                duration = parseFloat(duration) || 1;
+            duration = options.duration ?
+            (hAzzle.speeds[options.duration] || options.duration) :
+            defaultDuration;
+        
+		// Height/width overflow pass
+		
+        if (elem.nodeType === 1 && ('height' in to.to || 'width' in to.to)) {
+            this.originalState.overflow = [style.overflow, style.overflowX, style.overflowY];
+            display = hAzzle.css(elem, 'display');
+            checkDisplay = display === 'none' ?
+                hAzzle.getPrivate(elem, 'olddisplay') || defaultDisplay(elem.nodeName) : display;
+            if (checkDisplay === 'inline' && hAzzle.css(elem, 'float') === 'none') {
+                style.display = 'inline-block';
+            }
         }
 
-	if ( elem.nodeType === 1 && ( 'height' in to.to || 'width' in to.to ) ) {
-		this.originalState.overflow = [ style.overflow, style.overflowX, style.overflowY ];
-		display = hAzzle.css( elem, 'display' );
-		checkDisplay = display === 'none' ?
-			hAzzle.getPrivate( elem, 'olddisplay' ) || defaultDisplay( elem.nodeName ) : display;
-		if ( checkDisplay === 'inline' && hAzzle.css( elem, 'float' ) === 'none' ) {
-			style.display = 'inline-block';
-		}
-	}
-
-	if ( this.originalState.overflow ) {
-		style.overflow = 'hidden';
-	}
+        if (this.originalState.overflow) {
+            style.overflow = 'hidden';
+        }
 
         for (prop in from) {
 
             end = parseInt(newbs[prop], 10);
             begin = parseInt(from[prop], 10);
-
-            ar[i++] = [end > begin, prop, end, begin];
+            
+			// Create the array
+            
+			ar[i++] = [end > begin, prop, end, begin];
         }
 
         // Make visible if hidden
@@ -82,7 +70,8 @@ Percentage.prototype = {
 
         // Start the animation
 
-        if (!engineRunning) {
+        if (!isRunning) {
+			
             ticker();
         }
     },
@@ -98,17 +87,16 @@ Percentage.prototype = {
         var tick, timed = 0,
             then = pnow(),
             now, i, style = elem.style,
-            leg = to.length,
-            itm, begin;
+            len = to.length;
 
         return function(force) {
 
             now = pnow();
             timed += now - then;
             then = now;
-
             tick = hAzzle.easing[ease](timed / duration);
-            i = leg;
+			
+            i = len;
 
             if (tick < 0.99 && !force) {
 
@@ -117,9 +105,7 @@ Percentage.prototype = {
 
                 while (i--) {
 
-                    itm = to[i];
-
-                    if (to[i][0]) {
+                     if (to[i][0]) {
 
                         style[to[i][1]] = (to[i][3] + ((to[i][2] - to[i][3]) * tick)) + percent;
 
@@ -148,20 +134,20 @@ Percentage.prototype = {
     // Stop a percentage animation
 
     stop: function(complete, callback, popped) {
-		
-		var self = this,  
-		    state = self.originalState,
-			elem = self.elem,
-			transitions = self.transitions,
-			style = elem.style; 
-		   this.originalState = [];
-		
-		if ( state != null) {
-			
-            style.overflow = state.overflow[ 0 ];
-			style.overflowX = state.overflow[ 1 ];
-			style.overflowY = state.overflow[ 2 ];
-		}
+
+        var self = this,
+            state = self.originalState,
+            elem = self.elem,
+            transitions = self.transitions,
+            style = elem.style;
+        this.originalState = [];
+
+        if (state != null) {
+
+            style.overflow = state.overflow[0];
+            style.overflowX = state.overflow[1];
+            style.overflowY = state.overflow[2];
+        }
 
         hAzzle.removePrivate(elem, 'fxDta');
 
