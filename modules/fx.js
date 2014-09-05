9666f7137c124e86b4fd9cfd086d42e877a8ec08
KFlash <kflash123@gmail.com>
2014-09-05 17:31:22 +0800

    typo fix

--- modules/fx.js
+++ modules/fx.js
@@ -77,11 +77,9 @@ Tween.prototype = {
                 animate: function(currentTime, jumpToEnd) {
 
                     var i, delta = currentTime - self.start,
-                        options = self.options,
-                        hook,
-                        style = self.elem.style;
-
-                    self.currentTime = currentTime
+                        options = self.options, style = self.elem.style;
+                        
+						self.currentTime = currentTime;
 
                     if (delta > self.duration || jumpToEnd) {
 
@@ -571,19 +569,3 @@ hAzzle.fxAfter.scrollTop = hAzzle.fxAfter.scrollLeft = {
         }
     }
 };
-
-
-
-// this gets you the next hex in line according to a 'position'
-function nextColor(pos, start, finish) {
-    var r = [],
-        i, e, from, to
-    for (i = 0; i < 6; i++) {
-        from = Math.min(15, parseInt(start.charAt(i), 16))
-        to = Math.min(15, parseInt(finish.charAt(i), 16))
-        e = Math.floor((to - from) * pos + from)
-        e = e > 15 ? 15 : e < 0 ? 0 : e
-        r[i] = e.toString(16)
-    }
-    return '#' + r.join('')
-}
