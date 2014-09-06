01fbd3badbd50114a19a7d6f7222e3073d11afca
KFlash <kflash123@gmail.com>
2014-09-06 21:21:37 +0800

    cssCore.js additions and fixes

--- modules/csscore.js
+++ modules/csscore.js
@@ -1,7 +1,13 @@
-var cssCore = hAzzle.cssCore = {
-   has:  {}, // Feature / bug detection
-   normalize: {} // Normalize properties
-};
+var
+    cssProps = ['textShadow', 'opacity', 'clip', 'zIndex',
+        'flex', 'order', 'borderCollapse', 'animation', 'animationFillMode', 'animationDirection',
+        'animatioName', 'animationTimingFunction', 'animationPlayState', 'perspective', 'boxSizing',
+        'textOverflow', 'columns', 'borderRadius', 'boxshadow', 'borderImage'
+    ],
+    i = cssProps.length,
+    cssCore = hAzzle.cssCore = {
+        has: {}, // Feature / bug detection
+    };
 
 /* ============================ FEATURE / BUG DETECTION =========================== */
 
@@ -34,87 +40,81 @@ hAzzle.assert(function(div) {
     }
 });
 
+hAzzle.assert(function(div) {
 
-hAzzle.assert( function(div) {
-  
-  var support = {};
-	
-	 // Helper function to get the proper vendor property name.
-  // (`transition` => `WebkitTransition`)
-  function getVendorPropertyName(prop) {
-    // Handle unprefixed versions (FF16+, for example)
-    if (prop in div.style) return prop;
+    var support = {};
 
-    var prefixes = ['Moz', 'Webkit', 'O', 'ms'];
-    var prop_ = prop.charAt(0).toUpperCase() + prop.substr(1);
+    // Helper function to get the proper vendor property name.
+    // (`transition` => `WebkitTransition`)
+    function getVendorPropertyName(prop) {
+        // Handle unprefixed versions (FF16+, for example)
+        if (prop in div.style) return prop;
 
-    for (var i=0; i<prefixes.length; ++i) {
-      var vendorProp = prefixes[i] + prop_;
-      if (vendorProp in div.style) { return vendorProp; }
+        var prefixes = ['Moz', 'Webkit', 'O', 'ms'],
+            vendorProp, i = prefixes.length,
+            prop_ = prop.charAt(0).toUpperCase() + prop.substr(1);
+
+        while (i--) {
+            vendorProp = prefixes[i] + prop_;
+            if (vendorProp in div.style) {
+                return vendorProp;
+            }
+        }
     }
-  }
-	
-  // Helper function to check if transform3D is supported.
-  // Should return true for Webkits and Firefox 10+.
-  
-  function checkTransform3dSupport() {
-    div.style[support.transform] = '';
-    div.style[support.transform] = 'rotateY(90deg)';
-    return div.style[support.transform] !== '';
-  }
 
-   var isChrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+    // Helper function to check if transform3D is supported.
+    // Should return true for Webkits and Firefox 10+.
 
-  // Check for the browser's transitions support.
-  support.transition      = getVendorPropertyName('transition');
-  support.transitionDelay = getVendorPropertyName('transitionDelay');
-  support.transform       = getVendorPropertyName('transform');
-  support.transformOrigin = getVendorPropertyName('transformOrigin');
-  support.filter          = getVendorPropertyName('Filter');
-  support.transform3d     = checkTransform3dSupport();
-
-  var eventNames = {
-    'transition':       'transitionend',
-    'MozTransition':    'transitionend',
-    'OTransition':      'oTransitionEnd',
-    'WebkitTransition': 'webkitTransitionEnd',
-    'msTransition':     'MSTransitionEnd'
-  };
-
-  // Detect the 'transitionend' event needed.
-  var key, transitionEnd = support.transitionEnd = eventNames[support.transition] || null;
-
-  for (key in support) {
-    if (support.hasOwnProperty(key) && typeof cssCore[key] === 'undefined') {
-     cssCore[key] = support[key];
+    function checkTransform3dSupport() {
+        div.style[support.transform] = '';
+        div.style[support.transform] = 'rotateY(90deg)';
+        return div.style[support.transform] !== '';
     }
-  }
 
-  cssCore.boxShadow = getVendorPropertyName('boxshadow');
-  cssCore.borderRadius = getVendorPropertyName('borderRadius');
-  cssCore.borderImage = getVendorPropertyName('borderImage');    
+    // Check for the browser's transitions support.
+    support.transition = getVendorPropertyName('transition');
+    support.transitionDelay = getVendorPropertyName('transitionDelay');
+    support.transform = getVendorPropertyName('transform');
+    support.transformOrigin = getVendorPropertyName('transformOrigin');
+    support.filter = getVendorPropertyName('Filter');
+    support.transform3d = checkTransform3dSupport();
+
+    // Detect the 'transitionend' event needed.
+    var key;
+
+    for (key in support) {
+        if (support.hasOwnProperty(key) && typeof cssCore[key] === 'undefined') {
+            cssCore[key] = support[key];
+        }
+    }
+
+    // Detect support for other CSS properties
+
+    while (i--) {
+        if (getVendorPropertyName(cssProps[i])) {
+            cssCore[cssProps[i]] = getVendorPropertyName(cssProps[i]);
+        }
+    }
 });
 
-// Check if support translate3d
+ // Check if support translate3d
 
 hAzzle.assert(function(div) {
 
-var  has3d, t, transforms = {
-        'webkitTransform':'-webkit-transform',
-        'OTransform':'-o-transform',
-        'msTransform':'-ms-transform',
-        'MozTransform':'-moz-transform',
-        'transform':'transform'
+    var has3d, t, transforms = {
+        'webkitTransform': '-webkit-transform',
+        'OTransform': '-o-transform',
+        'msTransform': '-ms-transform',
+        'MozTransform': '-moz-transform',
+        'transform': 'transform'
     };
-	
-  for(t in transforms){
 
-        if( div.style[t] !== undefined ){
+    for (t in transforms) {
+
+        if (div.style[t] !== undefined) {
             div.style[t] = 'translate3d(1px,1px,1px)';
             has3d = window.getComputedStyle(div).getPropertyValue(transforms[t]);
         }
     }
-	cssCore.translate3d = (has3d !== undefined && has3d.length > 0 && has3d !== "none");
-
-	});
-
+    cssCore.translate3d = (has3d !== undefined && has3d.length > 0 && has3d !== "none");
+});
