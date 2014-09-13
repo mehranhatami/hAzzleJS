55047d5ffe0769deccdc8413bc7295dd8d1b625a
KFlash <kflash123@gmail.com>
2014-09-13 21:19:33 +0800

    cleanup

--- modules/curcss.js
+++ modules/curcss.js
@@ -5,15 +5,15 @@ var cHeightWidth = /^(height|width)$/i,
     cTopLeft = /top|left/i,
     cPrefix = 'CSS',
     computedValues = function(elem) {
-
+        var view = false;
         if (elem && elem !== window) {
 
             if (elem.ownerDocument !== undefined) {
-                var view = elem.ownerDocument.defaultView;
+                view = elem.ownerDocument.defaultView;
             }
-            return view && hAzzle.cssCore.has.ComputedStyle ? 
-                        (view.opener ? view.getComputedStyle(elem, null) :
-                        window.getComputedStyle(elem, null)) : elem.style;
+            return view && hAzzle.cssHas.ComputedStyle ?
+                (view.opener ? view.getComputedStyle(elem, null) :
+                    window.getComputedStyle(elem, null)) : elem.style;
         }
         return null;
     },
@@ -25,7 +25,7 @@ var cHeightWidth = /^(height|width)$/i,
         if (hAzzle.private(elem, cPrefix) === undefined) {
             return computedValues(elem);
 
-        // If the computedStyle object has yet to be cached, do so now.
+            // If the computedStyle object has yet to be cached, do so now.
         } else if (!hAzzle.private(elem, cPrefix).computedStyle) {
             computed = hAzzle.private(elem, cPrefix).computedStyle = computedValues(elem);
 
@@ -40,80 +40,80 @@ var cHeightWidth = /^(height|width)$/i,
 
     curCSS = function(elem, prop, force) {
 
-    var computedValue = 0,
-        toggleDisplay = false,
-        revertDisplay = function() {
-        if (toggleDisplay) {
-            setCSS(elem, 'display', 'none');
+        var computedValue = 0,
+            toggleDisplay = false,
+            revertDisplay = function() {
+                if (toggleDisplay) {
+                    setCSS(elem, 'display', 'none');
+                }
+            };
+
+        if (cWidthHeight.test(prop) && getCSS(elem, 'display') === 0) {
+            toggleDisplay = true;
+            setCSS(elem, 'display', hAzzle.getDisplayType(elem));
         }
+
+        if (!force) {
+
+            if (prop === 'height' &&
+                getCSS(elem, 'boxSizing').toString().toLowerCase() !== 'border-box') {
+
+                var contentBoxHeight = elem.offsetHeight -
+                    (parseFloat(getCSS(elem, 'borderTopWidth')) || 0) -
+                    (parseFloat(getCSS(elem, 'borderBottomWidth')) || 0) -
+                    (parseFloat(getCSS(elem, 'paddingTop')) || 0) -
+                    (parseFloat(getCSS(elem, 'paddingBottom')) || 0);
+                revertDisplay();
+
+                return contentBoxHeight;
+
+            } else if (prop === 'width' &&
+                getCSS(elem, 'boxSizing').toString().toLowerCase() !== 'border-box') {
+
+                var contentBoxWidth = elem.offsetWidth -
+                    (parseFloat(getCSS(elem, 'borderLeftWidth')) || 0) -
+                    (parseFloat(getCSS(elem, 'borderRightWidth')) || 0) -
+                    (parseFloat(getCSS(elem, 'paddingLeft')) || 0) -
+                    (parseFloat(getCSS(elem, 'paddingRight')) || 0);
+
+                revertDisplay();
+
+                return contentBoxWidth;
+            }
+        }
+
+        var computedStyle = getStyles(elem);
+
+        // IE and Firefox do not return a value for the generic borderColor -- they only return individual values for each border side's color.
+        // As a polyfill for querying individual border side colors, just return the top border's color.
+
+        if ((hAzzle.ie || hAzzle.isFirefox) && prop === 'borderColor') {
+            prop = 'borderTopColor';
+        }
+
+        if (hAzzle.ie === 9 && prop === 'filter') {
+            computedValue = computedStyle.getPropertyValue(prop);
+        } else {
+
+            computedValue = computedStyle[prop];
+        }
+
+        if (computedValue === '' || computedValue === null) {
+            computedValue = elem.style[prop];
+        }
+
+        revertDisplay();
+
+        if (computedValue === 'auto' && cToprbLeft.test(prop)) {
+
+            var position = curCSS(elem, 'position');
+
+            if (position === 'fixed' || (position === 'absolute' && cTopLeft.test(prop))) {
+                computedValue = hAzzle(elem).position()[prop] + 'px';
+            }
+        }
+        return computedValue;
     };
-
-    if (cWidthHeight.test(prop) && getCSS(elem, 'display') === 0) {
-        toggleDisplay = true;
-        setCSS(elem, 'display', hAzzle.getDisplayType(elem));
-    }
-
-    if (!force) {
-
-        if (prop === 'height' &&
-            getCSS(elem, 'boxSizing').toString().toLowerCase() !== 'border-box') {
-
-            var contentBoxHeight = elem.offsetHeight -
-                (parseFloat(getCSS(elem, 'borderTopWidth')) || 0) -
-                (parseFloat(getCSS(elem, 'borderBottomWidth')) || 0) -
-                (parseFloat(getCSS(elem, 'paddingTop')) || 0) -
-                (parseFloat(getCSS(elem, 'paddingBottom')) || 0);
-            revertDisplay();
-
-            return contentBoxHeight;
-
-        } else if (prop === 'width' &&
-            getCSS(elem, 'boxSizing').toString().toLowerCase() !== 'border-box') {
-
-            var contentBoxWidth = elem.offsetWidth -
-                (parseFloat(getCSS(elem, 'borderLeftWidth')) || 0) -
-                (parseFloat(getCSS(elem, 'borderRightWidth')) || 0) -
-                (parseFloat(getCSS(elem, 'paddingLeft')) || 0) -
-                (parseFloat(getCSS(elem, 'paddingRight')) || 0);
-
-            revertDisplay();
-
-            return contentBoxWidth;
-        }
-    }
-
-    var computedStyle = getStyles(elem);
-
-    // IE and Firefox do not return a value for the generic borderColor -- they only return individual values for each border side's color.
-    // As a polyfill for querying individual border side colors, just return the top border's color.
-
-    if ((hAzzle.ie || hAzzle.isFirefox) && prop === 'borderColor') {
-        prop = 'borderTopColor';
-    }
-
-    if (hAzzle.ie === 9 && prop === 'filter') {
-        computedValue = computedStyle.getPropertyValue(prop);
-    } else {
-
-        computedValue = computedStyle[prop];
-    }
-
-    if (computedValue === '' || computedValue === null) {
-        computedValue = elem.style[prop];
-    }
-
-    revertDisplay();
-
-    if (computedValue === 'auto' && cToprbLeft.test(prop)) {
-
-        var position = curCSS(elem, 'position');
-
-        if (position === 'fixed' || (position === 'absolute' && cTopLeft.test(prop))) {
-            computedValue = hAzzle(elem).position()[prop] + 'px';
-        }
-    }
-     return computedValue;
-};
 
 // Expose
 
--- modules/units.js
+++ modules/units.js
@@ -1,12 +1,34 @@
 // Wrap into a Object to avoid conflicts
-lrmpRegex = /^(left$|right$|margin|padding)/,
-    reafRegex = /^(relative|absolute|fixed)$/,
-    topbotRegex = /^(top|bottom)$/,
-    azRegex = /[%A-z]+$/,
-    rotskewRegex = /^(rotate|skew)/i,
-    transinpRegex = /(^(scale|scaleX|scaleY|scaleZ|alpha|flexGrow|flexHeight|zIndex|fontWeight)$)|((opacity|red|green|blue|alpha)$)/i,
+var urmpRegex = /^(left$|right$|margin|padding)/,
+    ureafRegex = /^(relative|absolute|fixed)$/,
+    utopbotRegex = /^(top|bottom)$/,
+    urotskewRegex = /^(rotate|skew)/i,
+    utransinpRegex = /(^(scale|scaleX|scaleY|scaleZ|alpha|flexGrow|flexHeight|zIndex|fontWeight)$)|((opacity|red|green|blue|alpha)$)/i,
 
-    callUnitConversionData = {
+    // Get unit types
+
+    getUnitType = function (property) {
+        
+    if (urotskewRegex.test(property)) {
+        return 'deg';
+    } else if (utransinpRegex.test(property)) {
+        return '';
+    } else {
+
+        if (hAzzle.unitless[property]) {
+            return '';
+        }
+
+        // Default to px for all other properties.
+        return 'px';
+    }
+},
+     // Calculate unit ratios
+
+    calculateUnitRatios = function(elem) {
+
+    // Temporary placeholder
+    var unitsContainer = {
         parent: null,
         position: null,
         fontSize: null,
@@ -16,201 +38,186 @@ lrmpRegex = /^(left$|right$|margin|padding)/,
         remToPx: null,
         vwToPx: null,
         vhToPx: null
-    }
-    /**
-     * Converts one unit to another
-     *
-     * @param {Object} target
-     *  @param {String} prop
-     * @param {String} returnUnit
-     *
-     */
+    },
+     sameRatioIndicators = {
+            parent: elem.parentNode || document.body,
+            position: getCSS(elem, 'position'),
+            fontSize: getCSS(elem, 'fontSize')
+        },
 
-    hAzzle.units = function(px, unit, elem, prop) {
+        // Determine if the same % ratio can be used. % is based on 
+        // the elem's position value and its parent's width and height dimensions.
 
-        if (unit === '' ||
-            unit === 'px') {
+        samePercentRatio = ((sameRatioIndicators.position === unitsContainer.position) &&
+            (sameRatioIndicators.parent === unitsContainer.parent)),
 
-            return px; // Don't waste time if there is no conversion to do.
+        // Determine if the same em ratio can be used. em is relative to the elem's fontSize.
+
+        sameEmRatio = (sameRatioIndicators.fontSize === unitsContainer.fontSize);
+
+    // Store these ratio indicators call-wide for the next elem to compare against.
+    unitsContainer.parent = sameRatioIndicators.parent;
+    unitsContainer.position = sameRatioIndicators.position;
+    unitsContainer.fontSize = sameRatioIndicators.fontSize;
+
+    var measurement = 100,
+        unitRatios = {};
+
+    if (!sameEmRatio || !samePercentRatio) {
+
+        var overflowxy = ['overflow', 'overflowX', 'overflowY'],
+            dim = ['minWidth', 'maxWidth', 'width', 'minHeight', 'maxHeight', 'height'],
+            i = overflowxy.length,
+            x = dim.length,
+            dummy = hAzzle.private(elem).isSVG ?
+            document.createElementNS('http://www.w3.org/TR/SVG/', 'rect') :
+            document.createElement('div');
+
+        hAzzle.styleCache(dummy);
+
+        sameRatioIndicators.parent.appendChild(dummy);
+
+        // To accurately and consistently calculate conversion ratios, the element's cascaded overflow and box-sizing are stripped.
+        // Similarly, since width/height can be artificially constrained by their min-/max- equivalents, these are controlled for as well.
+        // Note: Overflow must be also be controlled for per-axis since the overflow property overwrites its per-axis values.
+
+        while (i--) {
+            setCSS(dummy, overflowxy[i], 'hidden');
         }
 
-        if (unit === '%') {
+        setCSS(dummy, 'position', sameRatioIndicators.position);
+        setCSS(dummy, 'fontSize', sameRatioIndicators.fontSize);
+        setCSS(dummy, 'boxSizing', 'content-box');
 
-            if (lrmpRegex.test(prop)) {
+        // Width and height act as our proxy properties for measuring the horizontal and vertical % ratios.
 
-                prop = 'width';
-
-            } else if (topbotRegex.test(prop)) {
-
-                prop = 'height';
-            }
-
-            elem = reafRegex.test(curCSS(elem, 'position')) ?
-                elem.offsetParent : elem.parentNode;
-
-            if (elem) {
-
-                prop = parseFloat(curCSS(elem, prop));
-
-                if (prop !== 0) {
-
-                    return px / prop * 100;
-                }
-            }
-            return 0;
+        while (x--) {
+            setCSS(dummy, dim[x], measurement + '%');
         }
 
-        if (unit === 'em') {
+        // PaddingLeft arbitrarily acts as our proxy property for the em ratio.
 
-            return px / parseFloat(hAzzle.curCSS(elem, 'fontSize'));
-        }
+        setCSS(dummy, 'paddingLeft', measurement + 'em');
 
-        // The first time we calculate how many pixels there is in 1 meter
-        // for calculate what is 1 inch/cm/mm/etc.
+        // Divide the returned value by the measurement to get the ratio between 1% and 1px. Default to 1 since working with 0 can produce Infinite.
 
-        if (hAzzle.units.unity === undefined) {
+        unitRatios.lastpToPW = unitsContainer.lastpToPW = (parseFloat(getCSS(dummy, 'width', null, true)) || 1) / measurement;
 
-            var units = hAzzle.units.unity = {};
 
-            hAzzle.assert(function(div) {
+        unitRatios.lastToPH = unitsContainer.lastToPH = (parseFloat(getCSS(dummy, 'height', null, true)) || 1) / measurement;
+        unitRatios.emToPx = unitsContainer.lastEmToPx = (parseFloat(getCSS(dummy, 'paddingLeft')) || 1) / measurement;
 
-                div.style.width = '100cm';
-                document.body.appendChild(div);
-                units.mm = div.offsetWidth / 1000;
-            });
+        sameRatioIndicators.parent.removeChild(dummy);
 
-            units.cm = units.mm * 10;
-            units.in = units.cm * 2.54;
-            units.pt = units.in * 1 / 72;
-            units.pc = units.pt * 12;
-        }
+        // Avoid memory leak in IE
 
-        // If the unity specified is not recognized we return the value.
+        dummy = null;
 
-        unit = hAzzle.units.unity[unit];
-
-        return unit ? px / unit : px;
-    };
-
-hAzzle.getUnitType = function(property) {
-    if (rotskewRegex.test(property)) {
-        return 'deg';
     } else {
-      
-	  // Unitless properties
-     
-	 if(hAzzle.unitless[property]) {
-       return '';
-     }
-		
-        /* Default to px for all other properties. */
-        return 'px';
+        unitRatios.lastEmToPx = unitsContainer.lastEmToPx;
+        unitRatios.lastpToPW = unitsContainer.lastpToPW;
+        unitRatios.lastToPH = unitsContainer.lastToPH;
     }
-}
 
- // Calculate unit ratios
+    // Whereas % and em ratios are determined on a per-element basis, the rem unit only needs to be checked
+    // once per call since it's exclusively dependant upon document.body's fontSize. If this is the first time
+    // that calculateUnitRatios() is being run during this call, remToPx will still be set to its default value of null,
+    // so we calculate it now.
 
-     function calculateUnitRatios(elem) {
-
-        var sameRatioIndicators = {
-                parent: elem.parentNode || document.body,
-                position: getCSS(elem, 'position'),
-                fontSize: getCSS(elem, 'fontSize')
-            },
-
-            // Determine if the same % ratio can be used. % is based on 
-            // the elem's position value and its parent's width and height dimensions.
-
-            samePercentRatio = ((sameRatioIndicators.position === callUnitConversionData.position) &&
-                (sameRatioIndicators.parent === callUnitConversionData.parent)),
-
-            // Determine if the same em ratio can be used. em is relative to the elem's fontSize.
-
-            sameEmRatio = (sameRatioIndicators.fontSize === callUnitConversionData.fontSize);
-
-        // Store these ratio indicators call-wide for the next elem to compare against.
-        callUnitConversionData.parent = sameRatioIndicators.parent;
-        callUnitConversionData.position = sameRatioIndicators.position;
-        callUnitConversionData.fontSize = sameRatioIndicators.fontSize;
-
-        var measurement = 100,
-            unitRatios = {};
-
-        if (!sameEmRatio || !samePercentRatio) {
-
-            var overflowxy = ['overflow', 'overflowX', 'overflowY'],
-                dim = ['minWidth', 'maxWidth', 'width', 'minHeight', 'maxHeight', 'height'],
-                i = overflowxy.length,
-                x = dim.length,
-                dummy = hAzzle.private(elem).isSVG ?
-                document.createElementNS('http://www.w3.org/TR/SVG/', 'rect') :
-                document.createElement('div');
-
-            hAzzle.styleCache(dummy);
-            
-            sameRatioIndicators.parent.appendChild(dummy);
-
-            // To accurately and consistently calculate conversion ratios, the element's cascaded overflow and box-sizing are stripped.
-            // Similarly, since width/height can be artificially constrained by their min-/max- equivalents, these are controlled for as well.
-            // Note: Overflow must be also be controlled for per-axis since the overflow property overwrites its per-axis values.
-
-            while (i--) {
-                setCSS(dummy, overflowxy[i], 'hidden');
-            }
-
-            setCSS(dummy, 'position', sameRatioIndicators.position);
-            setCSS(dummy, 'fontSize', sameRatioIndicators.fontSize);
-            setCSS(dummy, 'boxSizing', 'content-box');
-
-            // Width and height act as our proxy properties for measuring the horizontal and vertical % ratios.
-
-            while (x--) {
-                setCSS(dummy, dim[x], measurement + '%');
-            }
-
-            // PaddingLeft arbitrarily acts as our proxy property for the em ratio.
-
-            setCSS(dummy, 'paddingLeft', measurement + 'em');
-
-            // Divide the returned value by the measurement to get the ratio between 1% and 1px. Default to 1 since working with 0 can produce Infinite.
-
-            unitRatios.lastpToPW = callUnitConversionData.lastpToPW = (parseFloat(getCSS(dummy, 'width', null, true)) || 1) / measurement;
-           
-            
-            unitRatios.lastToPH = callUnitConversionData.lastToPH = (parseFloat(getCSS(dummy, 'height', null, true)) || 1) / measurement;
-            unitRatios.emToPx = callUnitConversionData.lastEmToPx = (parseFloat(getCSS(dummy, 'paddingLeft')) || 1) / measurement;
-
-            sameRatioIndicators.parent.removeChild(dummy);
-            
-            // Avoid memory leak in IE
-
-           dummy = null;
-           
-        } else {
-            unitRatios.lastEmToPx = callUnitConversionData.lastEmToPx;
-            unitRatios.lastpToPW = callUnitConversionData.lastpToPW;
-            unitRatios.lastToPH = callUnitConversionData.lastToPH;
-        }
-
-        // Whereas % and em ratios are determined on a per-element basis, the rem unit only needs to be checked
-        // once per call since it's exclusively dependant upon document.body's fontSize. If this is the first time
-        // that calculateUnitRatios() is being run during this call, remToPx will still be set to its default value of null,
-        // so we calculate it now.
-
-        if (callUnitConversionData.remToPx === null) {
-            // Default to browsers' default fontSize of 16px in the case of 0.
-            callUnitConversionData.remToPx = parseFloat(getCSS(document.body, 'fontSize')) || 16;
-        }
-
-        // Similarly, viewport units are %-relative to the window's inner dimensions.
-
-        if (callUnitConversionData.vwToPx === null) {
-            callUnitConversionData.vwToPx = parseFloat(window.innerWidth) / 100;
-            callUnitConversionData.vhToPx = parseFloat(window.innerHeight) / 100;
-        }
-
-        unitRatios.remToPx = callUnitConversionData.remToPx;
-        unitRatios.vwToPx = callUnitConversionData.vwToPx;
-        unitRatios.vhToPx = callUnitConversionData.vhToPx;
-        return unitRatios;
+    if (unitsContainer.remToPx === null) {
+        // Default to browsers' default fontSize of 16px in the case of 0.
+        unitsContainer.remToPx = parseFloat(getCSS(document.body, 'fontSize')) || 16;
     }
+
+    // Similarly, viewport units are %-relative to the window's inner dimensions.
+
+    if (unitsContainer.vwToPx === null) {
+        unitsContainer.vwToPx = parseFloat(window.innerWidth) / 100;
+        unitsContainer.vhToPx = parseFloat(window.innerHeight) / 100;
+    }
+
+    unitRatios.remToPx = unitsContainer.remToPx;
+    unitRatios.vwToPx = unitsContainer.vwToPx;
+    unitRatios.vhToPx = unitsContainer.vhToPx;
+    return unitRatios;
+},
+
+/**
+ * Converts one unit to another
+ *
+ * @param {Object} target
+ * @param {String} prop
+ * @param {String} returnUnit
+ *
+ */
+
+units = function(px, unit, elem, prop) {
+
+    if (unit === '' ||
+        unit === 'px') {
+
+        return px; // Don't waste time if there is no conversion to do.
+    }
+
+    if (unit === '%') {
+
+        if (urmpRegex.test(prop)) {
+
+            prop = 'width';
+
+        } else if (utopbotRegex.test(prop)) {
+
+            prop = 'height';
+        }
+
+        elem = ureafRegex.test(curCSS(elem, 'position')) ?
+            elem.offsetParent : elem.parentNode;
+
+        if (elem) {
+
+            prop = parseFloat(curCSS(elem, prop));
+
+            if (prop !== 0) {
+
+                return px / prop * 100;
+            }
+        }
+        return 0;
+    }
+
+    if (unit === 'em') {
+
+        return px / parseFloat(hAzzle.curCSS(elem, 'fontSize'));
+    }
+
+    // The first time we calculate how many pixels there is in 1 meter
+    // for calculate what is 1 inch/cm/mm/etc.
+
+    if (hAzzle.units.unity === undefined) {
+
+        var units = hAzzle.units.unity = {};
+
+        hAzzle.assert(function(div) {
+
+            div.style.width = '100cm';
+            document.body.appendChild(div);
+            units.mm = div.offsetWidth / 1000;
+        });
+
+        units.cm = units.mm * 10;
+        units.in = units.cm * 2.54;
+        units.pt = units.in * 1 / 72;
+        units.pc = units.pt * 12;
+    }
+
+    // If the unity specified is not recognized we return the value.
+
+    unit = hAzzle.units.unity[unit];
+
+    return unit ? px / unit : px;
+};
+
+// Expose
+hAzzle.calculateUnitRatios = calculateUnitRatios;
+hAzzle.getUnitType = getUnitType;
+hAzzle.units = units;
