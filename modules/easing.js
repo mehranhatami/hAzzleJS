33599b1e3cef4b805d0b6d3af6c6f10c3d256ed5
KFlash <kflash123@gmail.com>
2014-09-17 09:34:23 +0800

    cleaned up easings

--- modules/easing.js
+++ modules/easing.js
@@ -1,29 +1,35 @@
 // easings.js
 var defaultEasing = 'swing',
+    supportFloat32Array = 'Float32Array' in window,
 
     // Default to a pleasant-to-the-eye easeOut (like native animations)
     generateStep = function(steps) {
         return function(p) {
             return Math.round(p * steps) * (1 / steps);
         };
-    }, // Generate bezier
+    },
+
+    // Bezier curve function generator. Copyright Gaetan Renaudeau.
+    // Modified for use with hAzzle
 
     generateBezier = function(mX1, mY1, mX2, mY2) {
-        var NEWTON_ITERATIONS = 4,
-            NEWTON_MIN_SLOPE = 0.001,
-            SUBDIVISION_PRECISION = 0.0000001,
-            SUBDIVISION_MAX_ITERATIONS = 10,
+        var nIterations = 4,
+            nMinSlope = 0.001,
+            sPrecision = 0.0000001,
+            sMaxIterations = 10,
             kSplineTableSize = 11,
             kSampleStepSize = 1.0 / (kSplineTableSize - 1.0),
-            float32ArraySupported = 'Float32Array' in window;
+            i = 0;
 
-        /* Must contain four arguments. */
+        // Must contain four arguments
+
         if (arguments.length !== 4) {
             return false;
         }
 
-        /* Arguments must be numbers. */
-        for (var i = 0; i < 4; ++i) {
+        // Arguments must be numbers.
+
+        for (; i < 4; ++i) {
             if (typeof arguments[i] !== 'number' || isNaN(arguments[i]) || !isFinite(arguments[i])) {
                 return false;
             }
@@ -35,102 +41,103 @@ var defaultEasing = 'swing',
         mX1 = Math.max(mX1, 0);
         mX2 = Math.max(mX2, 0);
 
-        var mSampleValues = float32ArraySupported ? new Float32Array(kSplineTableSize) : new Array(kSplineTableSize);
+        var mSampleValues = supportFloat32Array ?
+            new Float32Array(kSplineTableSize) :
+            new Array(kSplineTableSize),
+            A = function(aA1, aA2) {
+                return 1.0 - 3.0 * aA2 + 3.0 * aA1;
+            },
+            B = function(aA1, aA2) {
+                return 3.0 * aA2 - 6.0 * aA1;
+            },
+            C = function(aA1) {
+                return 3.0 * aA1;
+            },
+            calcBezier = function(aT, aA1, aA2) {
+                return ((A(aA1, aA2) * aT + B(aA1, aA2)) * aT + C(aA1)) * aT;
+            },
+            getSlope = function(aT, aA1, aA2) {
+                return 3.0 * A(aA1, aA2) * aT * aT + 2.0 * B(aA1, aA2) * aT + C(aA1);
+            },
+            newtonRaphsonIterate = function(aX, aGuessT) {
+                var i = 0,
+                    currentSlope, currentX;
+                for (; i < nIterations; ++i) {
+                    currentSlope = getSlope(aGuessT, mX1, mX2);
 
-        function A(aA1, aA2) {
-            return 1.0 - 3.0 * aA2 + 3.0 * aA1;
-        }
+                    if (currentSlope === 0.0) {
+                        return aGuessT;
+                    }
 
-        function B(aA1, aA2) {
-            return 3.0 * aA2 - 6.0 * aA1;
-        }
-
-        function C(aA1) {
-            return 3.0 * aA1;
-        }
-
-        function calcBezier(aT, aA1, aA2) {
-            return ((A(aA1, aA2) * aT + B(aA1, aA2)) * aT + C(aA1)) * aT;
-        }
-
-        function getSlope(aT, aA1, aA2) {
-            return 3.0 * A(aA1, aA2) * aT * aT + 2.0 * B(aA1, aA2) * aT + C(aA1);
-        }
-
-        function newtonRaphsonIterate(aX, aGuessT) {
-            for (var i = 0; i < NEWTON_ITERATIONS; ++i) {
-                var currentSlope = getSlope(aGuessT, mX1, mX2);
-
-                if (currentSlope === 0.0) return aGuessT;
-
-                var currentX = calcBezier(aGuessT, mX1, mX2) - aX;
-                aGuessT -= currentX / currentSlope;
-            }
-
-            return aGuessT;
-        }
-
-        function calcSampleValues() {
-            for (var i = 0; i < kSplineTableSize; ++i) {
-                mSampleValues[i] = calcBezier(i * kSampleStepSize, mX1, mX2);
-            }
-        }
-
-        function binarySubdivide(aX, aA, aB) {
-            var currentX, currentT, i = 0;
-
-            do {
-                currentT = aA + (aB - aA) / 2.0;
-                currentX = calcBezier(currentT, mX1, mX2) - aX;
-                if (currentX > 0.0) {
-                    aB = currentT;
-                } else {
-                    aA = currentT;
+                    currentX = calcBezier(aGuessT, mX1, mX2) - aX;
+                    aGuessT -= currentX / currentSlope;
                 }
-            } while (Math.abs(currentX) > SUBDIVISION_PRECISION && ++i < SUBDIVISION_MAX_ITERATIONS);
 
-            return currentT;
-        }
+                return aGuessT;
+            },
 
-        function getTForX(aX) {
-            var intervalStart = 0.0,
-                currentSample = 1,
-                lastSample = kSplineTableSize - 1;
+            calcSampleValues = function() {
+                var i = 0;
+                for (; i < kSplineTableSize; ++i) {
+                    mSampleValues[i] = calcBezier(i * kSampleStepSize, mX1, mX2);
+                }
+            },
 
-            for (; currentSample != lastSample && mSampleValues[currentSample] <= aX; ++currentSample) {
-                intervalStart += kSampleStepSize;
-            }
+            binarySubdivide = function(aX, aA, aB) {
+                var currentX, currentT, i = 0;
 
-            --currentSample;
+                do {
+                    currentT = aA + (aB - aA) / 2.0;
+                    currentX = calcBezier(currentT, mX1, mX2) - aX;
+                    if (currentX > 0.0) {
+                        aB = currentT;
+                    } else {
+                        aA = currentT;
+                    }
+                } while (Math.abs(currentX) > sPrecision && ++i < sMaxIterations);
 
-            var dist = (aX - mSampleValues[currentSample]) / (mSampleValues[currentSample + 1] - mSampleValues[currentSample]),
-                guessForT = intervalStart + dist * kSampleStepSize,
-                initialSlope = getSlope(guessForT, mX1, mX2);
+                return currentT;
+            },
 
-            if (initialSlope >= NEWTON_MIN_SLOPE) {
-                return newtonRaphsonIterate(aX, guessForT);
-            } else if (initialSlope == 0.0) {
-                return guessForT;
-            } else {
-                return binarySubdivide(aX, intervalStart, intervalStart + kSampleStepSize);
-            }
-        }
+            getTForX = function(aX) {
+                var intervalStart = 0.0,
+                    currentSample = 1,
+                    lastSample = kSplineTableSize - 1;
 
-        var _precomputed = false;
+                for (; currentSample != lastSample && mSampleValues[currentSample] <= aX; ++currentSample) {
+                    intervalStart += kSampleStepSize;
+                }
 
-        function precompute() {
-            _precomputed = true;
-            if (mX1 != mY1 || mX2 != mY2) calcSampleValues();
-        }
+                --currentSample;
 
-        var f = function(aX) {
-            if (!_precomputed) precompute();
-            if (mX1 === mY1 && mX2 === mY2) return aX;
-            if (aX === 0) return 0;
-            if (aX === 1) return 1;
+                var dist = (aX - mSampleValues[currentSample]) / (mSampleValues[currentSample + 1] - mSampleValues[currentSample]),
+                    guessForT = intervalStart + dist * kSampleStepSize,
+                    initialSlope = getSlope(guessForT, mX1, mX2);
 
-            return calcBezier(getTForX(aX), mY1, mY2);
-        };
+                if (initialSlope >= nMinSlope) {
+                    return newtonRaphsonIterate(aX, guessForT);
+                } else if (initialSlope == 0.0) {
+                    return guessForT;
+                } else {
+                    return binarySubdivide(aX, intervalStart, intervalStart + kSampleStepSize);
+                }
+            },
+
+            _precomputed = false,
+
+            precompute = function() {
+                _precomputed = true;
+                if (mX1 != mY1 || mX2 != mY2) calcSampleValues();
+            },
+
+            f = function(aX) {
+                if (!_precomputed) precompute();
+                if (mX1 === mY1 && mX2 === mY2) return aX;
+                if (aX === 0) return 0;
+                if (aX === 1) return 1;
+
+                return calcBezier(getTForX(aX), mY1, mY2);
+            };
 
         f.getControlPoints = function() {
             return [{
@@ -197,7 +204,7 @@ var defaultEasing = 'swing',
                 time_lapsed = 0,
                 tolerance = 1 / 10000,
                 DT = 16 / 1000,
-                have_duration, dt, last_state;
+                haveDuration, dt, last_state;
 
             tension = parseFloat(tension) || 500;
             friction = parseFloat(friction) || 20;
@@ -206,22 +213,22 @@ var defaultEasing = 'swing',
             initState.tension = tension;
             initState.friction = friction;
 
-            have_duration = duration !== null;
+            haveDuration = duration !== null;
 
-            /* Calculate the actual time it takes for this animation to complete with the provided conditions. */
-            if (have_duration) {
-                /* Run the simulation without a duration. */
+            // Calculate the actual time it takes for this animation to complete with the provided conditions.
+            if (haveDuration) {
+                // Run the simulation without a duration
                 time_lapsed = springRK4Factory(tension, friction);
-                /* Compute the adjusted time delta. */
+                // Compute the adjusted time delta
                 dt = time_lapsed / duration * DT;
             } else {
                 dt = DT;
             }
 
             while (true) {
-                /* Next/step function .*/
+                // Next/step function
                 last_state = springIntegrateState(last_state || initState, dt);
-                /* Store the position. */
+                // Store the position
                 path.push(1 + last_state.x);
                 time_lapsed += 16;
                 /* If the change threshold is reached, break. */
@@ -229,19 +236,14 @@ var defaultEasing = 'swing',
                     break;
                 }
             }
-
-            /* If duration is not defined, return the actual time required for completing this animation. Otherwise, return a closure that holds the
-               computed path and returns a snapshot of the position according to a given percentComplete. */
-            return !have_duration ? time_lapsed : function(percentComplete) {
+            return !haveDuration ? time_lapsed : function(percentComplete) {
                 return path[(percentComplete * (path.length - 1)) | 0];
             };
         };
     }()),
 
     getEasing = function(value, duration) {
-
         var easing = value;
-
         if (typeof value === 'string') {
             if (!easings[value]) {
                 easing = false;
@@ -276,10 +278,6 @@ var defaultEasing = 'swing',
         },
         circ: function(p) {
             return 1 - Math.sqrt(1 - p * p);
-        },
-        elastic: function(p) {
-            return p === 0 || p === 1 ? p :
-                -Math.pow(2, 8 * (p - 1)) * Math.sin(((p - 1) * 80 - 7.5) * Math.PI / 15);
         },
         back: function(p) {
             return p * p * (3 * p - 2);
@@ -325,7 +323,7 @@ var defaultEasing = 'swing',
         },
 
         easeInStrong: function(p) {
-            return (p == 0) ? 0 : Math.pow(2, 10 * (p - 1));
+            return (p === 0) ? 0 : Math.pow(2, 10 * (p - 1));
         },
 
         wobble: function(p) {
@@ -337,7 +335,7 @@ var defaultEasing = 'swing',
         },
 
         flicker: function(p) {
-            var p = p + (Math.random() - 0.5) / 5;
+            p = p + (Math.random() - 0.5) / 5;
             return easings.sinusoidal(p < 0 ? 0 : p > 1 ? 1 : p);
         },
         mirror: function(p) {
@@ -368,7 +366,7 @@ var defaultEasing = 'swing',
         },
 
         sineInOut: function(p) {
-            return -0.5 * (Math.cos(Math.PI * p) - 1)
+            return -0.5 * (Math.cos(Math.PI * p) - 1);
         },
 
         sineOut: function(p) {
