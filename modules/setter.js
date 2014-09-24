8e951e2b843a9f8dfbe971a3820d2315c956c859
KFlash <kflash123@gmail.com>
2014-09-25 06:57:21 +0800

    reducing the code size

--- modules/setter.js
+++ modules/setter.js
@@ -1,26 +1,22 @@
 // setter.js
 var setter = hAzzle.setter = function(elems, fn, key, value, chainable, eG, raw) {
 
-    var i = 0, l = elems.length, elem, bulk = key === null;
+    var i = 0,
+        len = elems.length,
+        bulk = key === null;
 
     // Set multiple values
 
     if (hAzzle.type(key) === 'object') {
-
         chainable = true;
-
         for (i in key) {
             setter(elems, fn, i, key[i], true, eG, raw);
         }
 
         // Sets one value
-
     } else if (typeof value !== 'undefined') {
-
         chainable = true;
-
-        if (typeof value !== 'function') {
-
+        if (!hAzzle.isFunction(value)) {
             raw = true;
         }
 
@@ -41,17 +37,13 @@ var setter = hAzzle.setter = function(elems, fn, key, value, chainable, eG, raw)
         }
 
         if (fn) {
-
-            while (l--) {
-                elem = elems[l];
-                fn(elems[l], key, raw ?
-                    value :
-                    value.call(elem, l, fn(elem, key)));
+            for (; i < len; i++) {
+                fn(elems[i], key, raw ? value : value.call(elems[i], i, fn(elems[i], key)));
             }
         }
     }
 
     return chainable ? elems : bulk ?
-        fn.call(elems) : l ?
+        fn.call(elems) : len ?
         fn(elems[0], key) : eG;
-}
+};
