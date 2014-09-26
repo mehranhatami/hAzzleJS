203e8fa5b5f9ac8351f94116da86838024d1192b
KFlash <kflash123@gmail.com>
2014-09-26 23:29:33 +0800

    glitches

--- modules/offset.js
+++ modules/offset.js
@@ -1,38 +1,37 @@
 //  offset.js
 hAzzle.Core.offset = function(options) {
-       if ( arguments.length ) {
-			return options === undefined ?
-				this :
-				this.each(function( i ) {
-					setOffset( this, options, i );
-				});
-		}
-    
+    if (arguments.length) {
+        return options === undefined ?
+            this :
+            this.each(function(elem, i) {
+                setOffset(elem, options, i);
+            });
+    }
+
     var elem = this[0];
-    
-    if (!elem || 
-      // Make sure it's not a disconnected DOM node 
-      !hAzzle.contains(document.documentElement, elem)) {
-    
-    return {
-        top: 0,
-        left: 0,
-        height: 0,
-        width: 0
-    };
-}
+
+    if (!elem ||
+        // Make sure it's not a disconnected DOM node 
+        !hAzzle.contains(document.documentElement, elem)) {
+
+        return {
+            top: 0,
+            left: 0,
+            height: 0,
+            width: 0
+        };
+    }
     var de = elem.ownerDocument.documentElement,
         bcr = elem.getBoundingClientRect(),
         scroll = getWindowScroll();
 
     return {
-        top:  bcr.top + scroll.y - Math.max(0, de && de.clientTop, document.body.clientTop),
+        top: bcr.top + scroll.y - Math.max(0, de && de.clientTop, document.body.clientTop),
         left: bcr.left + scroll.x - Math.max(0, de && de.clientLeft, document.body.clientLeft),
         height: elem.offsetHeight,
         width: elem.offsetWidth
     };
 };
-
 
 function getWindowScroll() {
     return {
@@ -41,50 +40,50 @@ function getWindowScroll() {
     };
 }
 
- function setOffset( elem, options, i ) {
-		var curPosition, curLeft, curCSSTop, curTop, curOffset, curCSSLeft, calculatePosition,
-			position = curCSS( elem, "position" ),
-			curElem = hAzzle( elem ),
-			props = {};
+function setOffset(elem, ops, i) {
+    var curPosition, curLeft, curCSSTop, curTop, curOffset, curCSSLeft, calculatePosition,
+        position = curCSS(elem, 'position'),
+        curElem = hAzzle(elem),
+        props = {};
 
-		// Set position first, in-case top/left are set even on static elem
-		if ( position === "static" ) {
-			elem.style.position = "relative";
-		}
+    // Set position first, in-case top/left are set even on static elem
+    if (position === 'static') {
+        elem.style.position = 'relative';
+    }
 
-		curOffset = curElem.offset();
-		curCSSTop = curCSS( elem, "top" );
-		curCSSLeft = curCSS( elem, "left" );
-		calculatePosition = ( position === "absolute" || position === "fixed" ) &&
-			( curCSSTop + curCSSLeft ).indexOf("auto") > -1;
+    curOffset = curElem.offset();
+    curCSSTop = curCSS(elem, 'top');
+    curCSSLeft = curCSS(elem, 'left');
+    calculatePosition = (position === 'absolute' || position === 'fixed') &&
+        (curCSSTop + curCSSLeft).indexOf('auto') > -1;
 
-		// Need to be able to calculate position if either
-		// top or left is auto and position is either absolute or fixed
-		if ( calculatePosition ) {
-			curPosition = curElem.position();
-			curTop = curPosition.top;
-			curLeft = curPosition.left;
+    // Need to be able to calculate position if either
+    // top or left is auto and position is either absolute or fixed
+    if (calculatePosition) {
+        curPosition = curElem.position();
+        curTop = curPosition.top;
+        curLeft = curPosition.left;
 
-		} else {
-			curTop = parseFloat( curCSSTop ) || 0;
-			curLeft = parseFloat( curCSSLeft ) || 0;
-		}
+    } else {
+        curTop = parseFloat(curCSSTop) || 0;
+        curLeft = parseFloat(curCSSLeft) || 0;
+    }
 
-		if ( hAzzle.isFunction( options ) ) {
-			options = options.call( elem, i, curOffset );
-		}
+    if (hAzzle.isFunction(ops)) {
+        ops = ops.call(elem, i, curOffset);
+    }
 
-		if ( options.top != null ) {
-			props.top = ( options.top - curOffset.top ) + curTop;
-		}
-		if ( options.left != null ) {
-			props.left = ( options.left - curOffset.left ) + curLeft;
-		}
+    if (ops.top != null) {
+        props.top = (ops.top - curOffset.top) + curTop;
+    }
+    if (ops.left != null) {
+        props.left = (ops.left - curOffset.left) + curLeft;
+    }
 
-		if ( "using" in options ) {
-			options.using.call( elem, props );
+    if ('using' in ops) {
+        sops.using.call(elem, props);
 
-		} else {
-			curElem.css( props );
-		}
-	}
+    } else {
+        curElem.css(props);
+    }
+}
