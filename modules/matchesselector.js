9a8f2e81f9761510026300468ddc13424f87f456
KFlash <kflash123@gmail.com>
2014-09-27 15:11:15 +0800

    match fix

--- modules/matchesselector.js
+++ modules/matchesselector.js
@@ -3,6 +3,7 @@ var docElem = document.documentElement,
     mQuickMatch = /^(\w*)(?:#([\w\-]+))?(?:\[([\w\-\=]+)\])?(?:\.([\w\-]+))?$/;
 
 hAzzle.matches = function( expr, elements ) {
+    
     var results = [], elem, i = elements.length;
     while(i--) {
        elem = elements[i];
@@ -15,6 +16,34 @@ return results;
 };
 
 hAzzle.matchesSelector = function( elem, expr ) {
-return elem.matches( expr );
 
+    if (typeof selector !== 'string') {
+        return null;
+    }
+    var i = 0,
+        len = context.length,
+        cl3 = selector.replace(':', '').toUpperCase(),
+        result = [];
+
+    if (!len) { // if no length
+
+        // We are here using the CL3 module, same as we do with Jiesa.
+        // No point in reinventing the wheel!!
+
+        return hAzzle.Expr[cl3] ? hAzzle.Expr[cl3](context) :
+            hAzzle.matchesSelector(context, selector);
+    }
+
+    // loop through
+
+    for (; i < len; i++) {
+
+        if (hAzzle.Expr[cl3] && hAzzle.Expr[cl3](context[i])) {
+            result.push(context[i]);
+        } else if (hAzzle.matchesSelector(context[i], selector)) {
+            result.push(context[i]);
+        }
+    }
+
+    return result;
 };
