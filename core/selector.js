ebf156efd3d6b3916bc0edd759c828382df0725e
KFlash <kflash123@gmail.com>
2014-11-03 21:53:48 +0800

    quick fix

---
+++ core/selector.js
@@ -0,0 +1,12 @@
+// selector.js
+// hAzzle main selector engine
+var hAzzle = window.hAzzle || (window.hAzzle = {});
+
+hAzzle.define('selector', function() {
+
+    var find = function(sel, ctx) {}
+    return {
+        find: find
+    };
+
+});
